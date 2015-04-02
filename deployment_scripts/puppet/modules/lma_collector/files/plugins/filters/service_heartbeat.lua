e6213c0241f761bac2753025621a3a83568d3616
Simon Pasquier <spasquier@mirantis.com>
2015-04-02 17:33:20 +0200

    WIP: Add service heartbeat metrics
    Change-Id: I2544926ffe2016de19c42cda5da8bb440c97a726

---
+++ deployment_scripts/puppet/modules/lma_collector/files/plugins/filters/service_heartbeat.lua
@@ -0,0 +1,59 @@
+-- Copyright 2015 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+require 'cjson'
+require 'string'
+require 'math'
+
+local timeout = read_config("timeout") or 30
+
+services = {}
+local floor = math.floor
+
+function process_message ()
+    local ts = floor(read_message("Timestamp")/1e6) -- in ms
+    local service_name = string.match(read_message("Fields[name]"), '^[^._]+')
+    local hostname = read_message("Fields[hostname]")
+    local key = string.format('%s.%s', hostname, service_name)
+
+    local service = services[key]
+    if host then
+        service.last_seen = ts
+    else
+        service = {last_seen = ts, status = 1, host = hostname, name = service_name}
+        services[key] = service
+    end
+    return 0
+end
+
+function timer_event(ns)
+    local current_time = floor(ns / 1e6) -- in ms
+    local datapoints = {}
+
+    for k, service in pairs(services) do
+        if current_time - service.last_seen > timeout * 1000 then
+            service.status = 0
+        else
+            service.status = 1
+        end
+        datapoints[#datapoints+1] = {
+            name = string.format('%s.%s.status', service.host, service.name),
+            columns = {"time", "value"},
+            points = {{service.last_seen, service.status}}
+        }
+    end
+
+    if #datapoints > 0 then
+        inject_payload("json", "influxdb", cjson.encode(datapoints))
+    end
+end
---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/metrics/service_heartbeat.pp
@@ -0,0 +1,23 @@
+class lma_collector::metrics::service_heartbeat (
+  $services,
+  $timeout  = lma_collector::params::heartbeat_timeout,
+) inherits lma_collector::params {
+  include lma_collector::service
+
+  validate_array($services)
+
+  if (size($services) > 0) {
+    $regexp = join(sort($services), '|')
+
+    heka::filter::sandbox { 'service_heartbeat':
+      config_dir      => $lma_collector::params::config_dir,
+      filename        => "${lma_collector::params::plugins_dir}/filters/service_heartbeat.lua",
+      message_matcher => join(['Fields[name] =~ /^', join(sort($services), '|'), '/'], ''),
+      ticker_interval => 10,
+      config          => {
+        timeout => $timeout,
+      },
+      notify          => Class['lma_collector::service'],
+    }
+  }
+}
