3a3e4b7611a8d1eac227e527027dbf7b649d4813
Swann Croiset <scroiset@mirantis.com>
2015-04-10 15:39:29 +0200

    Add neutron agent state metrics
    collect number of agents in state disabled, up or down.
    These metrics are collected from DB with collectd DBI plugin.
    
    Change-Id: I98295fda14585656c19f562d496dd0aabf962976

--- deployment_scripts/puppet/modules/lma_collector/manifests/collectd/dbi_services.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/collectd/dbi_services.pp
@@ -11,6 +11,14 @@ define lma_collector::collectd::dbi_services (
   include lma_collector::collectd::service
   $service = $title
 
+  if $service == 'nova' or $service == 'cinder' {
+    $type = 'services'
+  }elsif $service == 'neutron' {
+    $type = 'agents'
+  }else{
+    fail("${service} not supported")
+  }
+
   $plugin_conf_dir = $collectd::params::plugin_conf_dir
 
   package { $lma_collector::params::collectd_dbi_package:
@@ -19,7 +27,7 @@ define lma_collector::collectd::dbi_services (
     before   => File["${plugin_conf_dir}/dbi_${service}_services.conf"],
   }
 
-  file { "${plugin_conf_dir}/dbi_${service}_services.conf":
+  file { "${plugin_conf_dir}/dbi_${service}_${type}.conf":
     owner   => 'root',
     group   => $collectd::params::root_group,
     mode    => '0644',
