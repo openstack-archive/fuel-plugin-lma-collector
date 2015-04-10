0931f67eab558a8a0bc6cbee301b20355f54cb7b
Swann Croiset <scroiset@mirantis.com>
2015-04-10 11:07:00 +0200

    Add Nova service state metrics
    collect number of services in state disabled, up or down.
    These metrics are collected from DB with collectd DBI plugin.
    
    Change-Id: I546d9c14f1a0c2d5e3b3c96f8908d83d52720839

---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/collectd/dbi_services.pp
@@ -0,0 +1,25 @@
+define lma_collector::collectd::dbi_services (
+  $database         = undef,
+  $hostname         = 'localhost',
+  $username         = undef,
+  $password         = undef,
+  $dbname           = undef,
+  $report_interval  = 60,
+  $downtime_factor  = 2,
+){
+  include collectd::params
+  include lma_collector::collectd::service
+  $service = $title
+
+  $downtime = $report_interval * $downtime_factor
+
+  $plugin_conf_dir = $collectd::params::plugin_conf_dir
+
+  file { "${plugin_conf_dir}/dbi_${service}_services.conf":
+    owner   => 'root',
+    group   => $collectd::params::root_group,
+    mode    => '0644',
+    content => template('lma_collector/collectd_dbi_services.conf.erb'),
+    notify  => Class['lma_collector::collectd::service'],
+  }
+}
