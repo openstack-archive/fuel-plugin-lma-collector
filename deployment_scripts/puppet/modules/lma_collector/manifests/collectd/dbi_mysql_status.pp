dbcfaaf88520fa4f1a61e6aacb01d56d93c92048
Swann Croiset <scroiset@mirantis.com>
2015-04-13 17:57:18 +0200

    Add MySQL cluster metrics
    Change-Id: I7a36f412cc37523db0480d01b267ca13f1b2b263

---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/collectd/dbi_mysql_status.pp
@@ -0,0 +1,20 @@
+define lma_collector::collectd::dbi_mysql_status (
+  $database         = undef,
+  $hostname         = 'localhost',
+  $username         = undef,
+  $password         = undef,
+  $dbname           = undef,
+){
+  include collectd::params
+  include lma_collector::collectd::service
+
+  $plugin_conf_dir = $collectd::params::plugin_conf_dir
+  file { "${plugin_conf_dir}/dbi_mysql_status.conf":
+    owner   => 'root',
+    group   => $collectd::params::root_group,
+    mode    => '0644',
+    content => template('lma_collector/collectd_dbi_mysql_status.conf.erb'),
+    notify  => Class['lma_collector::collectd::service'],
+  }
+}
+
