d813e79f3460f10c2331bbcadfc7e3784a0ad76b
Swann Croiset <scroiset@mirantis.com>
2015-04-15 17:00:40 +0200

    Add service status metrics
    A first Heka filter catch all service related metrics to consolidate service states
    and periodically emit a message containing the whole information.
    
    A second Heka filter consume the previous message to compute status of Openstack
    services and emit 2 kind of messages:
    - metrics status: the general status of the service, HAproxy backend
      server status, and per service/agent status when available (nova, cinder, neutron).
    - annotations with status transition description, these annotations will
      be handled by a futur filter to fill the influxdb and enable annotation on
      Grafana graphs.
      To notice that annotations are only emitted by Heka filter from where the
      "vip__public" pacemaker resource is active to avoid duplicate annotations
      (in futur should be replaced by the "Scalable Message Router")
    
    The general status of a service depends on underlying metrics:
    - API checks: openstack.<service>.check_api
    - HAproxy backend states: haproxy.backend.<backend>.server.(up|down)
    - service/agent states for nova, cinder, neutron:
      openstack.<service>.(services|agents).(up|down)
    
    Status is one of OK, DEGRADED, DOWN, UNKNOWN.
    
    Change-Id: Ifa34edadce87e1ecdd131315462f80b49c7edd6d

--- deployment_scripts/puppet/modules/lma_collector/files/plugins/common/lma_utils.lua
+++ deployment_scripts/puppet/modules/lma_collector/files/plugins/common/lma_utils.lua
@@ -49,6 +49,34 @@ metric_type = {
 
 local default_severity = 7
 
+service_status_map = {
+    OK = 0,
+    DEGRADED = 1,
+    DOWN = 2,
+    UNKNOWN = 3,
+}
+
+service_status_to_label_map = {
+    [0] = 'OK',
+    [1] = 'DEGRADED',
+    [2] = 'DOWN',
+    [3] = 'UNKNOWN',
+}
+
+check_api_to_status_map = {
+    [0] = 2, -- DOWN
+    [1] = 0, -- UP
+    [2] = 3, -- UNKNOWN
+}
+
+function add_metric(datapoints, name, points)
+    datapoints[#datapoints+1] = {
+        name = name,
+        columns = {"time", "value" },
+        points = {points}
+    }
+end
+
 -- Parse a Syslog-based payload and update the Heka message
 -- Return true if successful, false otherwise
 function parse_syslog_message(grammar, payload, msg)
---
+++ deployment_scripts/puppet/modules/lma_collector/files/plugins/filters/service_accumulator_states.lua
@@ -0,0 +1,87 @@
+-- Copyright 2015 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+require 'cjson'
+require 'string'
+require 'math'
+local floor = math.floor
+local utils  = require 'lma_utils'
+
+services = {}
+vip_active_at = 0
+
+local payload_name = read_config('inject_payload_name') or 'service_status'
+
+function process_message ()
+    local ts = floor(read_message("Timestamp")/1e6) -- ms
+    local metric_name = read_message("Fields[name]")
+    local value = read_message("Fields[value]")
+    local name
+    local top_entry
+    local item_name
+    local group_name
+    local state
+
+    if string.find(metric_name, '^pacemaker.resource.vip__public') and value == 1 then
+         vip_active_at = ts
+         return 0
+    end
+
+    if string.find(metric_name, '%.up$') then
+        state = 'up'
+    elseif string.find(metric_name, '%.down$') then
+        state = 'down'
+    elseif string.find(metric_name, '%.disabled$') then
+        state = 'disabled'
+    end
+
+    if string.find(metric_name, '^openstack') then
+        name, group_name, item_name = string.match(metric_name, '^openstack%.([^._]+)%.([^._]+)%.([^._]+)')
+        top_entry = 'workers'
+        if not item_name then
+            name = string.match(metric_name, '^openstack%.([^.]+)%.check.api$')
+            top_entry = 'check_api'
+        end
+
+    elseif string.find(metric_name, '^haproxy%.backend') then
+        top_entry = 'haproxy'
+        group_name = 'api'
+        item_name = string.match(metric_name, '^haproxy%.backend%.([^._]+)%.servers')
+        name = string.match(item_name, '^([^-]+)')
+    end
+    if not name then
+      return -1
+    end
+
+    -- table initialization for the first time we see a service
+    if not services[name] then services[name] = {} end
+    if not services[name][top_entry] then services[name][top_entry] = {} end
+
+    local service = services[name]
+    local item = {last_seen=ts, value=value}
+    if item_name then
+        if not service[top_entry][item_name] then
+            service[top_entry][item_name] = {}
+        end
+        item.state=state
+        item.group_name=group_name
+        service[top_entry][item_name][state] = item
+    else
+        service[top_entry] = item
+    end
+    return 0
+end
+
+function timer_event(ns)
+    inject_payload('json', payload_name, cjson.encode({vip_active_at=vip_active_at, services=services}))
+end
