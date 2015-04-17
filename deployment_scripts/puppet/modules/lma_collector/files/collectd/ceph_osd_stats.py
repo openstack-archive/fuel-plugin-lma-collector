e241c73790eb972fdfa08bd753fc12fc61f2bc7a
Simon Pasquier <spasquier@mirantis.com>
2015-04-17 13:24:22 +0200

    Fix all collectd plugins against subprocess bug
    This change makes sure that all collectd Python plugins using the
    subprocess library set up the workaround required for Python <= 2.6.
    
    The original bug (and the workaround) are described at
    https://github.com/deniszh/collectd-iostat-python/issues/2
    
    Change-Id: If7f75e0b6391459ac26ee40498a982bb19c287e7

--- deployment_scripts/puppet/modules/lma_collector/files/collectd/ceph_osd_stats.py
+++ deployment_scripts/puppet/modules/lma_collector/files/collectd/ceph_osd_stats.py
@@ -44,6 +44,10 @@ class CephOSDStatsPlugin(base.CephBase):
 plugin = CephOSDStatsPlugin()
 
 
+def init_callback():
+    plugin.restore_sigchld()
+
+
 def config_callback(conf):
     plugin.config_callback(conf)
 
@@ -51,5 +55,6 @@ def config_callback(conf):
 def read_callback():
     plugin.read_callback()
 
+collectd.register_init(init_callback)
 collectd.register_config(config_callback)
 collectd.register_read(read_callback, INTERVAL)
