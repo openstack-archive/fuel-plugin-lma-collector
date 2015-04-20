3f60fee134f6a40b14991edd7ba27d5402e3243f
Swann Croiset <scroiset@mirantis.com>
2015-04-20 10:46:34 +0200

    Add Heka filter to handle annotation messages
    Messages matching Field.payload_type = 'json' and Field.payload_name'. = 'annotation'
    pass through the filter and a new message is emitted for the InfluxDB output plugin.
    
    Annotation message format is the following:
    msg =
      Payload = {
        time=current_time,
        name="An optional name to create influxdb series annotation[.<name>]",
        title="The annotation title",
        tag="A annotation tag",
        text="The annotation text",
      }
      Fields = {
        payload_type='json',
        payload_name='annotation',
      }
    
    Change-Id: If7be46fdf5fd3603f235a8ec31331f6c8118e5e0

---
+++ deployment_scripts/puppet/modules/lma_collector/files/plugins/filters/influxdb_annotation.lua
@@ -0,0 +1,59 @@
+-- Copyright 2015 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+require 'cjson'
+require 'string'
+require "os"
+
+local last_flush = os.time()
+local datapoints = {}
+local base_serie_name = 'annotation'
+
+local flush_count = read_config('flush_count') or 100
+local flush_interval = read_config('flush_interval') or 5
+
+function flush ()
+    local now = os.time()
+    if #datapoints > 0 and (#datapoints > flush_count or now - last_flush > flush_interval) then
+        inject_payload("json", "influxdb", cjson.encode(datapoints))
+
+        datapoints = {}
+        last_flush = now
+    end
+end
+
+function process_message ()
+    local ok, annotations = pcall(cjson.decode, read_message("Payload"))
+    if not ok then return -1 end
+
+    for _, anno in ipairs(annotations) do
+        if anno and anno.name then
+            serie_name = string.format('%s.%s', base_serie_name, anno.name)
+        else
+            serie_name = base_serie_name
+        end
+
+        datapoints[#datapoints+1] = {
+            name = serie_name,
+            columns = {"time", "title", "tag", "text"},
+            points = {{anno.time, anno.title, anno.tag, anno.text}}
+        }
+    end
+
+    flush()
+    return 0
+end
+
+function timer_event(ns)
+    flush()
+end
