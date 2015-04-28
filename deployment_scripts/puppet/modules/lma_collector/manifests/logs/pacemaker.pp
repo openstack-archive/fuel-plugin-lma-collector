1c300b6467db0a6b826b3c27fbfe652515600feb
Swann Croiset <scroiset@mirantis.com>
2015-04-28 10:27:27 +0200

    Fix Pacemaker log parsing
    The Pacemaker logs aren't consistent between corosync and pacemaker
    services: some are using the <PRI> preamble and other don't. As a
    consequence, Heka don't split messages but meld entries and in some
    cases messages are dropped due to message size too large (> 64K).
    The fix consists to treat each line as a single Heka message and use a
    syslog grammar fallback to handle entry logs without <PRI>, like it's
    done with system logs.
    
    Change-Id: Ia7a2f45054b301a2308f3293ae280d7d7f834d17

--- deployment_scripts/puppet/modules/lma_collector/manifests/logs/pacemaker.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/logs/pacemaker.pp
@@ -20,28 +20,20 @@ class lma_collector::logs::pacemaker {
     config_dir => $lma_collector::params::config_dir,
     filename   => "${lma_collector::params::plugins_dir}/decoders/generic_syslog.lua" ,
     config     => {
-      syslog_pattern => $lma_collector::params::syslog_pattern
+      syslog_pattern => $lma_collector::params::syslog_pattern,
+      fallback_syslog_pattern => $lma_collector::params::fallback_syslog_pattern
     },
     notify     => Class['lma_collector::service'],
   }
 
-  # Use the <PRI> token as the delimiter because Pacemaker may log messages
-  # with newlines and the configuration of the Syslog daemon doesn't escape
-  # them.
-  heka::splitter::regex { 'pacemaker':
-    config_dir    => $lma_collector::params::config_dir,
-    delimiter     => '(<[0-9]+>)',
-    delimiter_eol => false,
-    notify        => Class['lma_collector::service'],
-  }
-
+  # Use the default splitter 'TokenSplitter' with 'newline' delimiter,
+  # because Pacemaker may log messages with and without <PRI> preambule.
   heka::input::logstreamer { 'pacemaker':
     config_dir     => $lma_collector::params::config_dir,
     decoder        => 'pacemaker',
-    splitter       => 'pacemaker',
     file_match     => 'pacemaker\.log$',
     differentiator => '[ \'pacemaker\' ]',
-    require        => [Heka::Decoder::Sandbox['pacemaker'], Heka::Splitter::Regex['pacemaker']],
+    require        => Heka::Decoder::Sandbox['pacemaker'],
     notify         => Class['lma_collector::service'],
   }
 }
