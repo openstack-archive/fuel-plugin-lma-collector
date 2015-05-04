1fb59a80d54b9caa9ec61be1db6b3441de8c55db
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-05-04 16:57:07 +0200

    Add parameter to give the host for the mod_status module
    This change allows to pass a list of IPs that can be passed to the
    mod_status apache module. On MOS 6.0 horizon is listening on the
    management IP so the default parameter for mod_status (ie localhost) is
    not working. With this patch we can pass an array of allow IPs.
    
    Change-Id: I88644397c8c3074d5ab39f235406e5e137a6aff0

--- deployment_scripts/puppet/modules/lma_collector/manifests/mod_status.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/mod_status.pp
@@ -17,40 +17,39 @@
 # We don't use apache::mod_status because it requires to include the apache
 # base class. And by doing this we overwrite horizon configuration.
 
-class lma_collector::mod_status {
+class lma_collector::mod_status (
+  $allow_from = ['127.0.0.1'],
+) {
 
   include apache::params
   include apache::service
 
+  validate_array($allow_from)
+
   case $::osfamily {
     'redhat': {
+      $lib_path    = 'modules/'
       $status_conf = '/etc/httpd/conf.d/status.conf'
       $status_load = '/etc/httpd/conf.d/status.load'
-
-      file { $status_load:
-        source => 'puppet:///modules/lma_collector/apache/status.load'
-      }
-
-      file { $status_conf:
-        source  => 'puppet:///modules/lma_collector/apache/status.conf',
-        require => File[$status_load],
-        notify  => Class['apache::service'],
-      }
     }
 
     'debian': {
-      $status_conf = '/etc/apache2/mods-enabled/status.conf'
-      $status_load = '/etc/apache2/mods-enabled/status.load'
+      $lib_path    = '/usr/lib/apache2/modules/'
+      $status_conf = '/etc/apache2/mods-available/status.conf'
+      $status_load = '/etc/apache2/mods-available/status.load'
+      $status_conf_link = '/etc/apache2/mods-enabled/status.conf'
+      $status_load_link = '/etc/apache2/mods-enabled/status.load'
 
-      file { $status_conf:
-        ensure => link,
-        target => '/etc/apache2/mods-available/status.conf',
+      file { $status_conf_link:
+        ensure  => link,
+        target  => $status_conf,
+        require => File[$status_conf],
       }
 
-      file { $status_load:
+      file { $status_load_link:
         ensure  => link,
-        target  => '/etc/apache2/mods-available/status.load',
-        require => File[$status_conf],
+        target  => $status_load,
+        require => File[$status_load],
         notify  => Class['apache::service'],
       }
     }
@@ -59,4 +58,18 @@ class lma_collector::mod_status {
       notify {"Cannot enable apache status module on ${::operatingsystem}": }
     }
   }
+
+  # Template uses $allow_from, $lib_path
+  file { $status_conf:
+    ensure  => file,
+    content => template('lma_collector/apache/status.conf.erb'),
+    require => File[$status_load],
+    notify  => Class['apache::service'],
+  }
+
+  file { $status_load:
+    ensure  => file,
+    content => template('lma_collector/apache/status.load.erb'),
+  }
+
 }
