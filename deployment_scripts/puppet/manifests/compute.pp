a5263c9adf0717cd77f6707ea797a1d559a07e3e
Swann Croiset <scroiset@mirantis.com>
2015-05-22 11:58:16 +0200

    Add UI option to disable logs and notifications indexation
    This change also enables the collection of logs and notifications even
    if the events indexation is disabled. The reason is that useful metrics
    are extracted from this data such as HTTP response times, resource
    creation time and so on.
    The term 'event' is used to reference both logs and OpenStack notifcations.
    
    Change-Id: I28814129f745a8ef3ee44d3d3cf1cc267337b840

--- deployment_scripts/puppet/manifests/compute.pp
+++ deployment_scripts/puppet/manifests/compute.pp
@@ -17,19 +17,18 @@ include lma_collector::params
 $ceilometer    = hiera('ceilometer')
 $lma_collector = hiera('lma_collector')
 
-$enable_notifications = $lma_collector['enable_notifications']
-if $ceilometer['enabled'] {
-  $notification_topics = [$lma_collector::params::openstack_topic, $lma_collector::params::lma_topic]
-}
-else {
-  $notification_topics = [$lma_collector::params::lma_topic]
-}
+if $lma_collector['elasticsearch_mode'] != 'disabled' {
+  if $ceilometer['enabled'] {
+    $notification_topics = [$lma_collector::params::openstack_topic, $lma_collector::params::lma_topic]
+  }
+  else {
+    $notification_topics = [$lma_collector::params::lma_topic]
+  }
 
-class { 'lma_collector::logs::openstack': }
+  class { 'lma_collector::logs::openstack': }
 
-class { 'lma_collector::logs::libvirt': }
+  class { 'lma_collector::logs::libvirt': }
 
-if $enable_notifications {
   class { 'lma_collector::notifications::compute':
     topics  => $notification_topics,
   }
