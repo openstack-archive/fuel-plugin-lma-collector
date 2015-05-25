6250aa36212537bc0d72baae8eadf6233eedc397
Swann Croiset <scroiset@mirantis.com>
2015-05-25 15:55:28 +0200

    Cap CPU usage of Hekad
    By default the 'maxprocs' is set to 1 CPU every 12 CPU available on the host.
    
    Change-Id: I1b619bc5220d5d1a3f71d3c080b7f2f5f332fe9a

--- deployment_scripts/puppet/modules/lma_collector/manifests/params.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/params.pp
@@ -44,6 +44,11 @@ class lma_collector::params {
   # Lets configure 192Kb by default.
   $hekad_max_message_size = 196608
 
+  # The maximum number of CPU used by Heka,
+  # use by default one CPU per 12 CPU available.
+  # Set to 1 to have no limit
+  $hekad_maxprocs_per_unit = 1
+
   # Parameters for OpenStack notifications
   $rabbitmq_host = false
   $rabbitmq_port = '5672'
