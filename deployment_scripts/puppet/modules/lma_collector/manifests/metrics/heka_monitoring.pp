cb93085ea0982705c79576b138f876c7f93608ec
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-07-03 11:47:18 +0200

    Add the monitoring of Heka messages
    Add information about the number of messages that are processed by the
    toolchain and also the average time it takes to process these messages.
    We focused on filter and decoder.
    
    Change-Id: Iad122336fe8fcb6fa3f4d8ed666b276f6db70a43

---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/metrics/heka_monitoring.pp
@@ -0,0 +1,29 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+class lma_collector::metrics::heka_monitoring {
+  include lma_collector::service
+
+  heka::filter::sandbox { 'heka_monitoring':
+    config_dir      => $lma_collector::params::config_dir,
+    filename        => "${lma_collector::params::plugins_dir}/filters/heka_monitoring.lua",
+    message_matcher => "Type == 'heka.all-report'",
+    notify          => Class['lma_collector::service'],
+  }
+
+  # Dashboard is required to enable monitoring messages
+  heka::output::dashboard { 'dashboard':
+    config_dir => $lma_collector::params::config_dir,
+  }
+}
