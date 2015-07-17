5a8ecb3415b49fc6ddb9376ba6b04a346984ed70
Simon Pasquier <spasquier@mirantis.com>
2015-07-17 16:31:04 +0200

    Add encoder + accumulator for InfluxDB v0.9
    This change adds the initial implementation of the Heka output plugin
    for InfluxDB v0.9.
    
    First the InfluxDB accumulator catches all metric messages, buffers them
    and injects a payload message containing the datapoints encoded using
    the new InfluxDB line protocol. This message is injected either when the
    number of datapoints reaches the configured limit (currently 100) or
    after the configured time-out (currently 5 seconds). Eventually the
    InfluxDB output plugin catches the message and sends the payload as-is
    to the InfluxDB server thanks to the PayloadEncoder.
    
    Change-Id: I5297c6e894f3f7b6d680a1fae2b73cbd81b60746
    Implements: blueprint upgrade-influxdb-grafana

--- deployment_scripts/puppet/modules/lma_collector/files/plugins/filters/influxdb_accumulator.lua
+++ deployment_scripts/puppet/modules/lma_collector/files/plugins/filters/influxdb_accumulator.lua
@@ -11,45 +11,135 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-require "cjson"
-require "string"
-require "os"
-
-local last_flush = os.time()
-local datapoints = {}
+require 'os'
+require 'string'
+require 'table'
+local field_util = require 'field_util'
+local l = require 'lpeg'
+l.locale(l)
 
 local flush_count = read_config('flush_count') or 100
 local flush_interval = read_config('flush_interval') or 5
+local default_tenant_id = read_config("default_tenant_id")
+local default_user_id = read_config("default_user_id")
+local time_precision = read_config("time_precision")
 
+-- the tag_fields parameter is a list of tags separated by spaces
+local tag_grammar = l.Ct((l.C((l.P(1) - l.P" ")^1) * l.P" "^0)^0)
+local tag_fields = tag_grammar:match(read_config("tag_fields") or "")
+
+local defaults = {
+    tenant_id=default_tenant_id,
+    user_id=default_user_id,
+}
+local last_flush = os.time()
+local datapoints = {}
+
+function escape_string(str)
+    return tostring(str):gsub("([ ,])", "\\%1")
+end
+
+function encode_tag(tag, default_value)
+    local value = read_message(string.format('Fields[%s]', tag))
+
+    if not value or value == '' then
+        if default_value ~= nil then
+            value = default_value
+        else
+            return nil
+        end
+    end
+
+    return escape_string(tag) .. '=' .. escape_string(value)
+end
+
+-- Flush the datapoints to InfluxDB if enough items are present or if the
+-- timeout has expired
 function flush ()
     local now = os.time()
     if #datapoints > 0 and (#datapoints > flush_count or now - last_flush > flush_interval) then
-        inject_payload("json", "influxdb", cjson.encode(datapoints))
+        datapoints[#datapoints+1] = ''
+        inject_payload("txt", "influxdb", table.concat(datapoints, "\n"))
 
         datapoints = {}
         last_flush = now
     end
 end
 
-function process_message ()
-    local ts = read_message("Timestamp") / 1e6 -- InfluxDB defaults to ms
-    local hostname = read_message("Fields[hostname]") or read_message("Hostname")
-    local metric_name = read_message("Fields[name]")
+-- TODO(pasquier-s): support messages with multiple points
+-- TODO(pasquier-s): support points with multiple fields
+function process_message()
+    local point
+    local tags = {}
     local value = read_message("Fields[value]")
+    local ts
+    if time_precision  and time_precision ~= 'ns' then
+        ts = field_util.message_timestamp(time_precision)
+    else
+        ts = read_message('Timestamp')
+    end
 
-    if value == nil and metric_name == nil then
+    -- collect Fields[tag_fields]
+    local msg_tag_fields = {}
+    local i = 0
+    while true do
+        local t = read_message("Fields[tag_fields]", 0, i)
+        if not t then
+            break
+        end
+        table.insert(msg_tag_fields, t)
+        i = i + 1
+    end
+
+    if value == nil then
         return -1
     end
 
-    local serie_name = string.format('%s.%s', hostname, metric_name)
-    datapoints[#datapoints+1] = {
-        name = serie_name,
-        columns = {"time", "value"},
-        points = {{ts, value}}
-    }
-    flush()
+    -- Add common tags
+    for _, tag in ipairs(tag_fields) do
+        local t = encode_tag(tag, defaults[tag])
+        if t then
+            table.insert(tags, t)
+        end
+    end
 
-   return 0
+    -- Add specific tags
+    for _, tag in ipairs(msg_tag_fields) do
+        local t = encode_tag(tag)
+        if t then
+            table.insert(tags, t)
+        end
+    end
+
+    -- Encode the value field
+    if type(value) == "string" then
+        -- string values need to be double quoted
+        value = '"' .. value:gsub('"', '\\"') .. '"'
+    elseif type(value) == "number" or string.match(value, "^[%d.]+$") then
+        -- Always send numbers as formatted floats, so InfluxDB will accept
+        -- them if they happen to change from ints to floats between
+        -- points in time.  Forcing them to always be floats avoids this.
+        value = string.format("%.6f", value)
+    end
+
+    if #tags > 0 then
+        -- for performance reasons, it is recommended to always send the tags
+        -- in the same order.
+        table.sort(tags)
+        point = string.format("%s,%s value=%s %d",
+            escape_string(read_message('Fields[name]')),
+            table.concat(tags, ','),
+            value,
+            ts)
+    else
+        point = string.format("%s value=%s %d",
+            escape_string(read_message('Fields[name]')),
+            value,
+            ts)
+    end
+
+    datapoints[#datapoints+1] = point
+    return 0
 end
 
 function timer_event(ns)
