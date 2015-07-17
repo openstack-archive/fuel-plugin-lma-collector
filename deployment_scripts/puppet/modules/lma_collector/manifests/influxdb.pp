032d7317c4ceb120b6f7b78b06df5fe43fddfad0
Simon Pasquier <spasquier@mirantis.com>
2015-07-17 16:31:04 +0200

    Add encoder for InfluxDB v0.9
    This change adds the initial implementation of the Lua encoder for
    InfluxDB.
    
    Change-Id: I5297c6e894f3f7b6d680a1fae2b73cbd81b60746
    Implements: blueprint upgrade-influxdb-grafana

--- deployment_scripts/puppet/modules/lma_collector/manifests/influxdb.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/influxdb.pp
@@ -13,53 +13,39 @@
 #    under the License.
 #
 class lma_collector::influxdb (
-  $server   = $lma_collector::params::influxdb_server,
-  $port     = $lma_collector::params::influxdb_port,
-  $database = $lma_collector::params::influxdb_database,
-  $user     = $lma_collector::params::influxdb_user,
-  $password = $lma_collector::params::influxdb_password,
+  $server         = $lma_collector::params::influxdb_server,
+  $port           = $lma_collector::params::influxdb_port,
+  $database       = $lma_collector::params::influxdb_database,
+  $user           = $lma_collector::params::influxdb_user,
+  $password       = $lma_collector::params::influxdb_password,
+  $time_precision = $lma_collector::params::influxdb_time_precision,
 ) inherits lma_collector::params {
   include lma_collector::service
 
   validate_string($server)
 
-  heka::filter::sandbox { 'influxdb_accumulator':
-    config_dir      => $lma_collector::params::config_dir,
-    filename        => "${lma_collector::params::plugins_dir}/filters/influxdb_accumulator.lua",
-    message_matcher => 'Type == \'metric\' || Type == \'heka.sandbox.metric\'',
-    ticker_interval => 1,
-    config          => {
-      flush_interval => $lma_collector::params::influxdb_flush_interval,
-      flush_count    => $lma_collector::params::influxdb_flush_count,
-    },
-    notify          => Class['lma_collector::service'],
-  }
-
-  heka::filter::sandbox { 'influxdb_annotation':
-    config_dir      => $lma_collector::params::config_dir,
-    filename        => "${lma_collector::params::plugins_dir}/filters/influxdb_annotation.lua",
-    message_matcher => 'Type == \'heka.sandbox.status\' && Fields[updated] == TRUE',
-    ticker_interval => 1,
-    config          => {
-      flush_interval => $lma_collector::params::influxdb_flush_interval,
-      flush_count    => $lma_collector::params::influxdb_flush_count,
-    },
-    notify          => Class['lma_collector::service'],
-  }
-
-  heka::encoder::payload { 'influxdb':
+  heka::encoder::sandbox { 'influxdb':
     config_dir => $lma_collector::params::config_dir,
+    config     => {
+      tag_fields        => 'hostname deployment_id tenant_id user_id',
+      # FIXME(pasquier-s): provide the tenant_id & user_id parameters but this
+      # requires to request Keystone since we only have access to the tenant
+      # name and user name for services
+      default_tenant_id => 'to_be_fixed',
+      default_user_id   => 'to_be_fixed',
+    },
+    filename   => "${lma_collector::params::plugins_dir}/encoders/influxdb.lua",
     notify     => Class['lma_collector::service'],
   }
 
   heka::output::http { 'influxdb':
     config_dir      => $lma_collector::params::config_dir,
-    url             => "http://${server}:${port}/db/${database}/series",
-    message_matcher => 'Fields[payload_type] == \'json\' && Fields[payload_name] == \'influxdb\'',
+    url             => "http://${server}:${port}/write?db=${database}&precision=${time_precision}",
+    message_matcher => 'Type == \'metric\' || Type == \'heka.sandbox.metric\'',
     username        => $user,
     password        => $password,
     timeout         => $lma_collector::params::influxdb_timeout,
-    require         => [Heka::Encoder::Payload['influxdb'], Heka::Filter::Sandbox['influxdb_accumulator']],
+    require         => Heka::Encoder::Sandbox['influxdb'],
     notify          => Class['lma_collector::service'],
   }
 }
