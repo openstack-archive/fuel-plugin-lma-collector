openstack-archive/fuel-plugin-lma-collector

commit 254eda470b88f396695a3cfaf50709b8f78395b5

Fix typo in heka/manifests/init.pp
Change-Id: I26c0bc01166a456f208cc1f68dccd45ec6796ea2

deployment_scripts/puppet/modules/heka/manifests/init.pp
@@ -121,8 +121,7 @@ class heka (
     require => Package['heka'],
   }
 
-  if ! $run_as_root {
-    # TODO: check if this is really needed
+  if $run_as_root {
     user { $heka_user:
     }
   } else {

commit 252373a62691456771417ebfe8196818cdb85266

Fix issue on CentOS with Heka user
The deployment on CentOS is broken since 254eda4

This patch creates always the 'heka' user defined in heka::params:user
even if the Hekad process run as 'root'.

This way should works for both MOS 6.1 and 7.

Change-Id: I9ec690735b10f149d4477f0b8a7ca3a7d0cc54c1

deployment_scripts/puppet/modules/heka/manifests/init.pp
@@ -24,11 +24,6 @@
 # [*config_dir*]
 #   The directory where to store the configuration (default: '/etc/hekad').
 #
-# [*run_as_root*]
-#   Whether or not to run the Heka service as root (default: false).
-#   You may have to set this parameter to true on some systems to access log
-#   files, run additional commands, ...
-#
 # [*additional_groups*]
 #   Additional groups to which the heka user should be added.
 #
@@ -75,8 +70,6 @@
 class heka (
   $service_name = $heka::params::service_name,
   $config_dir = $heka::params::config_dir,
-  $heka_user = $heka::params::user,
-  #$run_as_root = $heka::params::run_as_root,
   $additional_groups = $heka::params::additional_groups,
   $hostname = $heka::params::hostname,
   $maxprocs = $heka::params::maxprocs,
@@ -89,10 +82,10 @@ class heka (
   $internal_statistics = $heka::params::internal_statistics,
 ) inherits heka::params {
 
-  $run_as_root   = $heka_user == 'root'
   $hekad_wrapper = "/usr/local/bin/${service_name}_wrapper"
   $base_dir      = "/var/cache/${service_name}"
   $log_file      = "/var/log/${service_name}.log"
+  $heka_user     = $heka::params::user
 
   package { $heka::params::package_name:
     ensure => present,
@@ -106,7 +99,7 @@ class heka (
       command => '/etc/init.d/heka stop',
       onlyif  => '/usr/bin/test -f /etc/init.d/heka',
       require => Package['heka'],
-      before  => User['heka'],
+      before  => User[$heka_user],
       notify  => Exec['disable_heka_daemon']
     }
 
@@ -121,18 +114,13 @@ class heka (
     require => Package['heka'],
   }
 
-  if $run_as_root {
-    user { $heka_user:
-    }
-  } else {
-    user { $heka_user:
-      shell   => '/sbin/nologin',
-      home    => $base_dir,
-      system  => true,
-      groups  => $additional_groups,
-      alias   => 'heka',
-      require => Package['heka'],
-    }
+  user { $heka_user:
+    shell   => '/sbin/nologin',
+    home    => $base_dir,
+    system  => true,
+    groups  => $additional_groups,
+    alias   => 'heka',
+    require => Package['heka'],
   }
 
   file { $base_dir:
@@ -140,7 +128,7 @@ class heka (
     owner   => $heka_user,
     group   => $heka_user,
     mode    => '0750',
-    require => [User['heka'], Package['heka']],
+    require => [User[$heka_user], Package['heka']],
   }
 
   file { $config_dir:
@@ -148,7 +136,7 @@ class heka (
     owner   => $heka_user,
     group   => $heka_user,
     mode    => '0750',
-    require => [User['heka'], Package['heka']],
+    require => [User[$heka_user], Package['heka']],
   }
 
   file { $log_file:
@@ -156,7 +144,7 @@ class heka (
     owner   => $heka_user,
     group   => $heka_user,
     mode    => '0660',
-    require => [User['heka'], Package['heka']],
+    require => [User[$heka_user], Package['heka']],
   }
 
   file { "/etc/logrotate.d/${service_name}":

deployment_scripts/puppet/modules/lma_collector/manifests/init.pp
@@ -56,7 +56,6 @@ class lma_collector (
   class { 'heka':
     service_name        => $service_name,
     config_dir          => $config_dir,
-    heka_user           => $heka_user,
     additional_groups   => union($lma_collector::params::groups, $groups),
     hostname            => $::hostname,
     pre_script          => $pre_script,