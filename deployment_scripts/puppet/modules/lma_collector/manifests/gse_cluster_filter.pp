eaf0c5004dcbf5d366c9f599d832338e0d5947dd
Simon Pasquier <spasquier@mirantis.com>
2015-09-01 16:58:25 +0200

    [WIP] Configure the GSE cluster service filter
    This change configures the GSE cluster service filter. This isn't a
    complete implementation yet in the sense that not all service
    relationships are described.
    
    Change-Id: I25d21c4f3ad2ea57175faecdfbe4b93561f621d8
    Implements: blueprint alerting-lma-collector

---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/gse_cluster_filter.pp
@@ -0,0 +1,60 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+define lma_collector::gse_cluster_filter (
+  $input_message_types,
+  $entity_field,
+  $output_message_type,
+  $output_metric_name,
+  $interval = 10,
+  $level_1_dependencies = {},
+  $level_2_dependencies = {},
+  $ensure = present,
+) {
+  include lma_collector::params
+  include heka::params
+
+  validate_array($input_message_types)
+  validate_string($entity_field)
+  validate_string($output_metric_name)
+  if size($input_message_types) == 0 {
+    fail('input_message_types cannot be empty')
+  }
+
+  $lua_modules_dir = $heka::params::lua_modules_dir
+  $topology_file = "gse_${title}_topology"
+  $message_matcher = inline_template('<%= @input_message_types.collect{|x| "Type == \'heka.sandbox.#{x}\'"}.join(" || ") %>')
+
+  heka::filter::sandbox { "gse_${title}":
+    config_dir      => $lma_collector::params::config_dir,
+    filename        => "${lma_collector::params::plugins_dir}/filters/gse_cluster_filter.lua",
+    message_matcher => $message_matcher,
+    ticker_interval => 1,
+    config          => {
+      output_message_type => $output_message_type,
+      output_metric_name  => $output_metric_name,
+      source              => "gse_${title}_filter",
+      interval            => $interval,
+      topology_file       => $topology_file,
+      entity_field        => $entity_field,
+    },
+    require         => File[$topology_file],
+    notify          => Class['lma_collector::service']
+  }
+
+  file { $topology_file:
+    ensure  => present,
+    path    => "${lua_modules_dir}/${topology_file}.lua",
+    content => template('lma_collector/gse_topology.lua.erb'),
+  }
+}
---
+++ deployment_scripts/puppet/modules/lma_collector/spec/defines/gse_cluster_filter_spec.rb
@@ -0,0 +1,46 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+require 'spec_helper'
+
+describe 'lma_collector::gse_cluster_filter' do
+    let(:title) { :service }
+    let(:facts) do
+        {:kernel => 'Linux', :operatingsystem => 'Ubuntu',
+         :osfamily => 'Debian'}
+    end
+
+    describe 'with defaults' do
+        let(:params) do
+            {:input_message_types => ['afd_service_metric'],
+             :entity_field => 'service',
+             :output_message_type => 'gse_service_cluster_status',
+             :output_metric_name => 'cluster_service_status'}
+        end
+        it { is_expected.to contain_heka__filter__sandbox('gse_service').with_message_matcher("Type == 'heka.sandbox.afd_service_metric'") }
+    end
+
+    describe 'with dependencies' do
+        let(:params) do
+            {:input_message_types => ['afd_service_metric', 'afd_node_metric'],
+             :entity_field => 'service',
+             :output_message_type => 'gse_service_cluster_status',
+             :output_metric_name => 'cluster_service_status',
+             :level_1_dependencies => {'nova' => ['nova-api','nova-scheduler'],
+                                       'cinder' => ['cinder-api']},
+             :level_2_dependencies => {'nova-api' => ['neutron-api']}
+            }
+        end
+        it { is_expected.to contain_heka__filter__sandbox('gse_service').with_message_matcher("Type == 'heka.sandbox.afd_service_metric' || Type == 'heka.sandbox.afd_node_metric'") }
+    end
+end
