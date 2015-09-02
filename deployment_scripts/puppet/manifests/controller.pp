adbeed886a50c96be198c2811a97cadf05bee757
Simon Pasquier <spasquier@mirantis.com>
2015-09-02 15:09:40 +0200

    [WIP] Configure the AFD plugins
    This change configures the Anomaly and Fault Detection (AFD) filter
    plugins on the controller nodes.
    
    Change-Id: Ifa2a923b070383b79a104b93a33076cb13d74ab8
    Implements: blueprint alerting-lma-collector

--- deployment_scripts/puppet/manifests/controller.pp
+++ deployment_scripts/puppet/manifests/controller.pp
@@ -161,6 +161,10 @@ if $lma_collector['influxdb_mode'] != 'disabled' {
   # Service status metrics and annotations
   class { 'lma_collector::metrics::service_status': }
 
+  # AFD filters
+  class { 'lma_collector::afd::api_endpoints': }
+  class { 'lma_collector::afd::workers': }
+
 }
 
 $alerting_mode = $lma_collector['alerting_mode']
