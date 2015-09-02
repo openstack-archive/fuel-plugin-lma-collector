72c4f69e40724c768573e01cfd7ef42293788434
Simon Pasquier <spasquier@mirantis.com>
2015-09-02 14:35:28 +0200

    [WIP] Add AFD plugins for endpoints and workers
    This change introduces the Anomaly and Fault Detection (AFD) filter
    plugins that return AFD events for the API endpoints and service
    workers (eg, nova-scheduler, nova-conductor, ...).
    
    Change-Id: I75bfb433e4e174659900f885040a1c2032efd470
    Implements: blueprint alerting-lma-collector

---
+++ deployment_scripts/puppet/modules/lma_collector/files/plugins/filters/afd_api_backends.lua
@@ -0,0 +1,97 @@
+-- Copyright 2015 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local afd = require 'afd'
+local consts = require 'gse_constants'
+
+local haproxy_backend_states = {}
+local check_api_states = {}
+
+-- emit AFD event metrics based on HAProxy backends and check_api metrics
+function process_message()
+    local metric_name = read_message('Fields[name]')
+    local service = read_message('Fields[service]')
+    local value = read_message('Fields[value]')
+
+    if not haproxy_backend_states[service] then
+        haproxy_backend_states[service] = {up=0, down=0}
+    end
+
+    if metric_name == 'haproxy_backend_servers' then
+        if not haproxy_backend_states[service] then
+            haproxy_backend_states[service] = {}
+        end
+        local state = read_message('Fields[state]')
+        haproxy_backend_states[service][state] = value
+    else
+        check_api_states[service] = value
+    end
+
+    if not check_api_states[service] then
+        return 0
+    end
+
+    local state = consts.OKAY
+    if not (haproxy_backend_states[service].up and not haproxy_backend_states[service].down) then
+        if not check_api_states[service] then
+            -- not enough data for now
+            return 0
+        end
+    elseif haproxy_backend_states[service].up == 0 then
+        state = consts.DOWN
+        afd.add_to_alarms(consts.DOWN,
+                          'last',
+                          string.format("%s[service=%s,state=up]", metric_name, service),
+                          '==',
+                          0,
+                          nil,
+                          nil,
+                          string.format("All %s endpoints are down", service))
+    elseif haproxy_backend_states[service].down >= haproxy_backend_states[service].up then
+        state = consts.CRIT
+        afd.add_to_alarms(consts.CRIT,
+                          'last',
+                          string.format("%s[service=%s,state=down]", metric_name, service),
+                          '>=',
+                          haproxy_backend_states[service].up,
+                          nil,
+                          nil,
+                          string.format("The number of down endpoints for %s is greater than or equal to the number of up endpoints", service))
+    elseif haproxy_backend_states[service].down > 0 then
+        state = consts.WARN
+        afd.add_to_alarms(consts.WARN,
+                          'last',
+                          string.format("%s[service=%s,state=down]", metric_name, service),
+                          '>',
+                          0,
+                          nil,
+                          nil,
+                          string.format("%d %s endpoints(s) is/are down", haproxy_backend_states[service].down, service))
+    end
+
+    if check_api_states[service] and check_api_states[service] == 0 then
+        state = consts.DOWN
+        afd.add_to_alarms(consts.DOWN,
+                          'last',
+                          metric_name,
+                          '==',
+                          0,
+                          nil,
+                          nil,
+                          string.format("Service check for service %s is failed", service))
+    end
+
+    afd.inject_afd_service_event(service, state, 0, 'afd_api')
+    return 0
+end
---
+++ deployment_scripts/puppet/modules/lma_collector/tests/lua/test_afd.lua
@@ -0,0 +1,67 @@
+-- Copyright 2015 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+require('luaunit')
+package.path = package.path .. ";files/plugins/common/?.lua"
+
+-- mock the inject_message() function from the Heka sandbox library
+local last_injected_msg
+function inject_message(msg)
+    last_injected_msg = msg
+end
+
+local afd = require('afd')
+
+TestAfd = {}
+
+    function TestAfd:setUp()
+        afd.reset_alarms()
+    end
+
+    function TestAfd:test_add_to_alarms()
+        afd.add_to_alarms('crit', 'last', 'metric_1', '==', 0, nil, nil, "crit message")
+        local alarms = afd.get_alarms()
+        assertEquals(alarms[1].metric, 'metric_1')
+        assertEquals(alarms[1].message, 'crit message')
+
+        afd.add_to_alarms('warn', 'last', 'metric_2', '>=', 2, 5, 600, "warn message")
+        alarms = afd.get_alarms()
+        assertEquals(alarms[2].metric, 'metric_2')
+        assertEquals(alarms[2].message, 'warn message')
+    end
+
+    function TestAfd:test_inject_afd_service_event_without_alarms()
+        afd.inject_afd_service_event('nova-scheduler', 'okay', 10, 'some_source')
+
+        local alarms = afd.get_alarms()
+        assertEquals(#alarms, 0)
+        assertEquals(last_injected_msg.Type, 'afd_service_metric')
+        assertEquals(last_injected_msg.Fields.value, 'okay')
+        assertEquals(last_injected_msg.Payload, '{"alarms":[]}')
+    end
+
+    function TestAfd:test_inject_afd_service_event_with_alarms()
+        afd.add_to_alarms('crit', 'last', 'metric_1', '==', 0, nil, nil, "crit message")
+        afd.inject_afd_service_event('nova-scheduler', 'crit', 10, 'some_source')
+
+        local alarms = afd.get_alarms()
+        assertEquals(#alarms, 0)
+        assertEquals(last_injected_msg.Type, 'afd_service_metric')
+        assertEquals(last_injected_msg.Fields.value, 'crit')
+        assert(last_injected_msg.Payload:match('crit message'))
+    end
+
+lu = LuaUnit
+lu:setVerbosity( 1 )
+os.exit( lu:run() )
