b5e3305d4bb8854b09473dc3d654325d994b9950
Simon Pasquier <spasquier@mirantis.com>
2015-09-11 16:43:55 +0200

    Send GSE and AFD metrics to InfluxDB
    This change modifies the message matcher of the InfluxDB accumulator to
    catch the metrics emitted by the GSE and AFD filters. Since the AFD and
    GSE metrics are compatible with the legacy metrics, the accumulator
    itself doesn't need to be updated.
    
    Change-Id: I09aeb4194e321db95ca03e395d9b786bd8124f1a
    Implements: blueprint alerting-lma-collector

--- deployment_scripts/puppet/modules/lma_collector/manifests/influxdb.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/influxdb.pp
@@ -27,7 +27,7 @@ class lma_collector::influxdb (
   heka::filter::sandbox { 'influxdb_accumulator':
     config_dir      => $lma_collector::params::config_dir,
     filename        => "${lma_collector::params::plugins_dir}/filters/influxdb_accumulator.lua",
-    message_matcher => 'Type == \'metric\' || Type == \'heka.sandbox.metric\' || Type == \'heka.sandbox.bulk_metric\' || Type == \'heka.sandbox.multivalue_metric\'',
+    message_matcher => $lma_collector::params::influxdb_message_matcher,
     ticker_interval => 1,
     config          => {
       flush_interval => $lma_collector::params::influxdb_flush_interval,
