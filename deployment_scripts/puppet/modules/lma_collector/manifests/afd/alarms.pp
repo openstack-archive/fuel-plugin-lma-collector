b724d15d87a204dae0b473c85984267ad359e45b
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-09-22 13:15:00 +0200

    Generate alarms in lua and heka filters from yaml
    This commit creates two new functions:
      - get_cluster_names() that returns a list of cluster node names
        associated to a list of role passed as parameter.
      - get_cluster_alarms() that returns a hash table with a key that is
        the name of file that will contains alarm and the list of alarms to
        be added in this file.
    
    Change-Id: I933b0b82aa3be25f60380c28a34849e78df0f177

---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/afd/alarms.pp
@@ -0,0 +1,96 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+define lma_collector::afd::alarms (
+    $type,
+    $cluster_name,
+    $logical_name,
+    $alarms,
+    $message_matcher,
+) {
+    include lma_collector::params
+    include lma_collector::service
+
+    $lua_template = inline_template('
+local M = {}
+setfenv(1, M) -- Remove external access to contain everything in the module
+
+local alarms = {
+<% @alarms.each do |alarm_name| -%>
+<% @alarms_definitions.each do |alarm| -%>
+<% if alarm_name == alarm["name"] -%>
+  {
+    [\'name\'] = \'<%= alarm_name %>\',
+    [\'description\'] = \'<%= alarm["description"] %>\',
+    [\'severity\'] = \'<%= alarm["severity"] %>\',
+    [\'trigger\'] = {
+<% if alarm["trigger"].key?("logical_operator") -%>
+      [\'logical_operator\'] = \'<%= alarm["trigger"]["logical_operator"] %>\',
+<% else -%>
+      [\'logical_operator\'] = \'or\',
+<% end -%>
+      [\'rules\'] = {
+<% alarm["trigger"]["rules"].each do |rule|  -%>
+        {
+          [\'metric\'] = \'<%= rule["metric"] %>\',
+<% if rule["fields"] -%>
+          [\'fields\'] = {
+<% rule["fields"].each do |k, v| -%>
+            [\'<%= k %>\'] = \'<%= v %>\',
+<% end -%>
+          },
+<% else -%>
+          [\'fields\'] = {},
+<% end -%>
+          [\'relational_operator\'] = \'<%= rule["relational_operator"] %>\',
+          [\'threshold\'] = \'<%= rule["threshold"] %>\',
+          [\'window\'] = \'<%= rule["window"] %>\',
+          [\'periods\'] = \'<%= rule["periods"] ||= 0 %>\',
+          [\'function\'] = \'<%= rule["function"] %>\',
+        },
+<% end -%>
+      },
+    },
+  },
+<% end -%>
+<% end -%>
+<% end -%>
+}
+
+return alarms
+')
+
+    # Create lua structures that describes alarms
+    file { "${alarms_dir}/lma_alarms_${name}.lua":
+      ensure => present,
+      content => inline_template($lua_template),
+    }
+
+    # Create the confguration file for Heka
+    heka::filter::sandbox { "afd_${type}_${cluster_name}_${logical_name}":
+      config_dir      => $lma_collector::params::config_dir,
+      filename        => "${lma_collector::params::plugins_dir}/filters/afd.lua",
+      message_matcher => "(Type == \'metric\' || Type == \'heka.sandbox.metric\') && (${message_matcher})",
+      ticker_interval => 10,
+      config          => {
+        afd_type         => $type,
+        afd_cluster_name => $cluster_name,
+        afd_logical_name => $logical_name,
+      },
+      notify          => Class['lma_collector::service'],
+    }
+}
+
+
