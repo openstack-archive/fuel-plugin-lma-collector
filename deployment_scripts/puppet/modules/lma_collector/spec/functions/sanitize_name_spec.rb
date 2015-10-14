ece13649531cd4c9b1893b283fd51ea0274296e7
Guillaume Thouvenin <gthouvenin@mirantis.com>
2015-10-14 10:51:32 +0200

    Fix alarm files named with hyphens
    If we generate an alarm for nova-api, the filter will have the option
    'afd_file' set to 'lma_alarms_nova-api_...' and this has some unexpected
    consequences when calling the Lua require() function. So we replace it
    by 'lma_alarms_nova_api...'
    
    Change-Id: I39d864bde1a81aae30d31a8f12f81439d6d1be98

---
+++ deployment_scripts/puppet/modules/lma_collector/spec/functions/sanitize_name_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'sanitize_name' do
+    it { is_expected.not_to eq(nil) }
+    it { is_expected.to run.with_params().and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
+    it { is_expected.to run.with_params('un', 'deux').and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
+    it { is_expected.to run.with_params('').and_return('')}
+    it { is_expected.to run.with_params('one-two-three').and_return('one_two_three')}
+end
