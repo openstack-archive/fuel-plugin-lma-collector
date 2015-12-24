7fbed026e9e600d7245414ec7603dd36645736de
Éric Lemoine <elemoine@mirantis.com>
2015-12-24 17:36:39 +0100

    Move Pacemaker/Corosync code out of lma_collector
    Change-Id: I0ed0bddb245dc3a65b034e5caec14a65cfa908cb
    Implements: blueprint lma-without-fuel

--- deployment_scripts/puppet/manifests/base.pp
+++ deployment_scripts/puppet/manifests/base.pp
@@ -104,10 +104,100 @@ if $is_controller{
 }
 
 class { 'lma_collector':
-  tags              => merge($tags, $additional_tags),
-  groups            => $additional_groups,
-  pacemaker_managed => $pacemaker_managed,
-  rabbitmq_resource => $rabbitmq_resource,
+  tags   => merge($tags, $additional_tags),
+  groups => $additional_groups,
+}
+
+# Now declare the lma_collector::service class. On controller nodes the LMA
+# collector service is managed by Pacemaker/Corosync, so setting up the service
+# involves declaring Pacemaker/Corosync related resources.
+if $pacemaker_managed {
+
+  include lma_collector::params
+  include heka::params
+
+  validate_string($rabbitmq_resource)
+
+  if $lma_collector::params::run_as_root {
+    $heka_user = 'root'
+  } else {
+    $heka_user = $heka::params::user
+  }
+
+  $service_name = $lma_collector::params::service_name
+  $config_dir = $lma_collector::params::config_dir
+
+  file { 'ocf-lma_collector':
+    ensure => present,
+    path   => '/usr/lib/ocf/resource.d/fuel/ocf-lma_collector',
+    source => 'puppet:///modules/lma_collector/ocf-lma_collector',
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+  }
+
+  cs_resource { $service_name:
+    primitive_class => 'ocf',
+    provided_by     => 'fuel',
+    primitive_type  => 'ocf-lma_collector',
+    complex_type    => 'clone',
+    ms_metadata     => {
+      # the resource should start as soon as the dependent resources (eg
+      # RabbitMQ) are running *locally*
+      'interleave'          => true,
+      'migration-threshold' => '3',
+      'failure-timeout'     => '120',
+    },
+    parameters      => {
+      'config'   => $config_dir,
+      'log_file' => "/var/log/${service_name}.log",
+      'user'     => $heka_user,
+    },
+    operations      => {
+      'monitor' => {
+        'interval' => '20',
+        'timeout'  => '10',
+      },
+      'start'   => {
+        'timeout' => '30',
+      },
+      'stop'    => {
+        'timeout' => '30',
+      },
+    },
+    require         => [File['ocf-lma_collector'], Class['heka']],
+  }
+
+  cs_rsc_colocation { "${service_name}-with-rabbitmq":
+    ensure     => present,
+    alias      => $service_name,
+    primitives => ["clone_${service_name}", $rabbitmq_resource],
+    score      => 0,
+    require    => Cs_resource[$service_name],
+  }
+
+  cs_rsc_order { "${service_name}-after-rabbitmq":
+    ensure  => present,
+    alias   => $service_name,
+    first   => $rabbitmq_resource,
+    second  => "clone_${service_name}",
+    # Heka cannot start if RabbitMQ isn't ready to accept connections. But
+    # once it is initialized, it can recover from a RabbitMQ outage. This is
+    # why we set score to 0 (interleave) meaning that the collector should
+    # start once RabbitMQ is active but a restart of RabbitMQ
+    # won't trigger a restart of the LMA collector.
+    score   => 0,
+    require => Cs_rsc_colocation[$service_name]
+  }
+
+  class { 'lma_collector::service':
+    provider => 'pacemaker',
+    require  => Cs_rsc_order[$service_name]
+  }
+
+} else {
+  # Use the default service class
+  include lma_collector::service
 }
 
 if $elasticsearch_mode != 'disabled' {
