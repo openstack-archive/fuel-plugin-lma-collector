f4ed406158cdbb9467c7460eac1914e25d67eda8
Éric Lemoine <elemoine@mirantis.com>
2015-12-24 17:31:29 +0100

    Make lma_collector::service inherit from ::params
    This is a typical pattern for parameterized classes, and we already use that
    pattern for the lma_collector class.
    
    This pattern is documented in the official Puppet documentation:
    https://docs.puppetlabs.com/puppet/latest/reference/lang_classes.html#inheritance
    
    Change-Id: I4d17366514a3d39c4a4d5231fea05fa03da9b6df

--- deployment_scripts/puppet/modules/lma_collector/manifests/service.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/service.pp
@@ -24,16 +24,12 @@
 #
 #
 class lma_collector::service (
-  $service_name = $::lma_collector::params::service_name,
+  $service_name = $lma_collector::params::service_name,
   $service_enable = true,
   $service_ensure = 'running',
   $service_manage = true,
   $provider       = undef,
-) {
-  # The base class must be included first because parameter defaults depend on it
-  if ! defined(Class['lma_collector::params']) {
-    fail('You must include the lma_collector::params class before using lma_collector::service')
-  }
+) inherits lma_collector::params {
 
   validate_bool($service_enable)
   validate_bool($service_manage)
