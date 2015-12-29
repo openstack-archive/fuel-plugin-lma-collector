63cfb6fcac3105d68e31d75ef6572eb22070ae6d
Éric Lemoine <elemoine@mirantis.com>
2015-12-29 17:53:39 +0100

    [WIP] Refactor the lma_collectod::collectd Puppet code
    Change-Id: I401158c638d074e7bd3ea353d8e0a4591f9ec958

---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/collectd/hypervisor.pp
@@ -0,0 +1,45 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+class lma_collector::collectd::hypervisor (
+  $user,
+  $password,
+  $tenant,
+  $keystone_url,
+  $timeout = $lma_collector::params::openstack_client_timeout,
+  $cpu_allocation_ratio = $lma_collector::params::nova_cpu_allocation_ratio,
+  $pacemaker_master_resource = undef,
+) inherits lma_collector::params {
+
+  include lma_collector::collectd::python_openstack_base
+
+  $real_config = {
+    'Username'           => "\"${user}\"",
+    'Password'           => "\"${password}\"",
+    'Tenant'             => "\"${tenant}\"",
+    'KeystoneUrl'        => "\"${keystone_url}\"",
+    'Timeout'            => "\"${timeout}\"",
+    'CpuAllocationRatio' => "\"${cpu_allocation_ratio}\"",
+  }
+
+  if $pacemaker_master_resource {
+    $real_config['DependsOnResource'] = "\"${pacemaker_master_resource}\""
+  }
+
+  lma_collector::collectd::python { 'hypervisor_stats':
+    config => $real_config,
+  }
+
+}
