e7ee2478545f7fcd989076e5b055b65fb5a8553d
Éric Lemoine <elemoine@mirantis.com>
2015-12-31 16:34:24 +0100

    Fix OpenStack logstreamer differentiator
    Using "/" as the separator in the Heka logstreamer differentiator
    prevents the logstreamer from creating a journal file. This is
    because the logstreamer uses the Logger name produced by the
    differentiator as the journal file name.
    
    This change fixes the problem by changing the differentiator separator
    from "/" to "_".
    
    Change-Id: Iaf838f0e5df370df24f0d9d63a9bf13e37aa4bbe
    Closes-Bug: #1530326

--- deployment_scripts/puppet/modules/lma_collector/manifests/logs/openstack.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/logs/openstack.pp
@@ -37,7 +37,7 @@ define lma_collector::logs::openstack {
     decoder        => 'openstack',
     splitter       => 'openstack',
     file_match     => '(?P<Service>.+)\.log\.?(?P<Seq>\d*)$',
-    differentiator => "['${title}', '/', 'Service']",
+    differentiator => "['${title}', '_', 'Service']",
     priority       => '["^Seq"]',
     require        => Class['lma_collector::logs::openstack_decoder_splitter'],
     notify         => Class['lma_collector::service'],
