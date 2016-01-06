6cf48afe8c6fe0566d4d4d59e534c25d073b969f
Swann Croiset <scroiset@mirantis.com>
2016-01-06 18:01:41 +0100

    Monitor Elasticsearch cluster
    The patch add a new manifest executed on both influxdb and elasticsearch
    nodes to configure collectd specially for them and also move here related
    configuration from base.pp.
    
    Implements: blueprint elasticsearch-clustering
    
    Change-Id: I0e75446dd97e8c7108be87513a2b13e6909fcf44

---
+++ deployment_scripts/puppet/manifests/lma_backends.pp
@@ -0,0 +1,47 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+$lma_collector_hash = hiera_hash('lma_collector')
+
+if $lma_collector_hash['influxdb_mode'] != 'disabled' {
+  $network_metadata = hiera('network_metadata')
+  $current_roles    = hiera('roles')
+
+  $is_elasticsearch_node = member($current_roles, 'elasticsearch_kibana') or member($current_roles, 'primary-elasticsearch_kibana')
+  $is_influxdb_node = member($current_roles, 'influxdb_grafana') or member($current_roles, 'primary-influxdb_grafana')
+
+  if $is_elasticsearch_node {
+    $process_matches = [{name => 'elasticsearch', regex => 'java'}]
+  } else {
+    $process_matches = undef
+  }
+
+  if $is_influxdb_node {
+    $processes = ['influxd', 'grafana-server', 'hekad', 'collectd']
+  } else {
+    $processes = ['hekad', 'collectd']
+  }
+
+  class { 'lma_collector::collectd::base':
+    processes       => $processes,
+    process_matches => $process_matches,
+  }
+
+  if $is_elasticsearch_node {
+    class { 'lma_collector::collectd::elasticsearch':
+      address => hiera('lma::elasticsearch::vip'),
+    }
+  }
+}
