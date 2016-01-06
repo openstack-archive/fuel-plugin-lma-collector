ab056e1f32e5c77117032e5154378f24638f613b
Swann Croiset <scroiset@mirantis.com>
2016-01-06 18:01:41 +0100

    Monitor Elasticsearch cluster
    Change-Id: I0e75446dd97e8c7108be87513a2b13e6909fcf44

---
+++ deployment_scripts/puppet/manifests/lma_backends.pp
@@ -0,0 +1,26 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+$lma_collector_hash = hiera_hash('lma_collector')
+
+if $lma_collector_hash['influxdb_mode'] != 'disabled' {
+  $network_scheme = hiera('network_scheme')
+  $es_vip_name = 'es_vip_mgmt'
+  $elasticsearch_vip = $network_metadata['vips'][$es_vip_name]['ipaddr']
+
+  class { 'lma_collector::collectd::elasticsearch':
+    address => $elasticsearch_vip,
+  }
+}
