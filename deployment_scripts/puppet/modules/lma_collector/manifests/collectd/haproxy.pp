dc485f32b2c2be186a5f10fb0e20cc067b6c63b8
Éric Lemoine <elemoine@mirantis.com>
2016-01-11 14:55:06 +0100

    Add lma_collector::collectd::haproxy
    This adds the lma_collector::collectd::haproxy class, which
    configures collectd for monitoring HAProxy.
    
    Change-Id: I48f3ed15f338fea15f6f8e97e9ba94c60a93861b

---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/collectd/haproxy.pp
@@ -0,0 +1,30 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+class lma_collector::collectd::haproxy (
+  $socket,
+) {
+
+  include lma_collector::params
+
+  lma_collector::collectd::python { 'haproxy':
+    config => {
+      'Socket'      => $socket,
+      'Mapping'     => $lma_collector::params::haproxy_names_mapping,
+      # Ignore internal stats ('Stats' for 6.1, 'stats' for 7.0) and lma proxies
+      'ProxyIgnore' => ['Stats', 'stats', 'lma'],
+    },
+  }
+}
