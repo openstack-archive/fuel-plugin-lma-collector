29c23a524848b7dbc65871e6c2940728d16deea8
Éric Lemoine <elemoine@mirantis.com>
2016-01-12 17:04:17 +0100

    Change lma_collector::collectd::ceph_osd
    This changes lma_collector::collectd::ceph_osd to use the new
    lma_collector:collectd::python defined type.
    
    Change-Id: Icc242b43c361dcecce52a0c59310ff3285a825ff

--- deployment_scripts/puppet/modules/lma_collector/manifests/collectd/ceph_osd.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/collectd/ceph_osd.pp
@@ -1,4 +1,4 @@
-#    Copyright 2015 Mirantis, Inc.
+#    Copyright 2015-2016 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -12,30 +12,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 #
-class lma_collector::collectd::ceph_osd
-{
-  include lma_collector::params
-  include collectd::params
 
-  $python_module_path = $lma_collector::params::python_module_path
+class lma_collector::collectd::ceph_osd {
 
-  $modules = {
-    'ceph_osd_perf' => {
-      'AdminSocket'   => '/var/run/ceph/ceph-*.asok',
+  lma_collector::collectd::python { 'ceph_osd_perf':
+    config => {
+      'AdminSocket' => '/var/run/ceph/ceph-*.asok',
     },
-  }
-  file {"${collectd::params::plugin_conf_dir}/ceph-osd.conf":
-    owner   => 'root',
-    group   => $collectd::params::root_group,
-    mode    => '0644',
-    content => template('lma_collector/collectd_python.conf.erb'),
-    notify  => Service['collectd'],
-  }
-
-  lma_collector::collectd::python_script { 'base.py':
-  }
-
-  lma_collector::collectd::python_script { 'ceph_osd_perf.py':
   }
 
 }
