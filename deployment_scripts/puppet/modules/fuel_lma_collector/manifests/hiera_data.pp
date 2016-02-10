63dadbc1ef7de9735c2a9abef27731c9a0389660
Simon Pasquier <spasquier@mirantis.com>
2016-02-10 11:44:09 +0100

    Create fuel_lma_collector module
    This change introduces the fuel_lma_collector Puppet module. This new
    module contains all the code which was in the lma_collector module and
    relies on the way Fuel uses Puppet like the use of Hiera.
    
    Change-Id: Ibfe97e745742b3f9718f873a8d564ed87476f334
    Implements: blueprint lma-without-fuel

--- deployment_scripts/puppet/modules/fuel_lma_collector/manifests/hiera_data.pp
+++ deployment_scripts/puppet/modules/fuel_lma_collector/manifests/hiera_data.pp
@@ -1,4 +1,4 @@
-#    Copyright 2015 Mirantis, Inc.
+#    Copyright 2016 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -11,7 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-define lma_collector::hiera_data (
+define fuel_lma_collector::hiera_data (
   $content,
   $ensure = present,
 ) {
