5c0a5dde450622004fdcdfbb2f98e3a941f22385
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-03-10 14:22:37 +0100

    Use OpenStack API to collect the status of Cinder workers
    This change uses the Cinder API to get the status of the cinder workers
    instead of querying the MySQL database.
    
    Change-Id: If92596b3cee8a4c9f0dcf84454fdff2a2532160f
    Partial-Bug: #1546188

--- deployment_scripts/puppet/modules/lma_collector/files/collectd/openstack_cinder.py
+++ deployment_scripts/puppet/modules/lma_collector/files/collectd/openstack_cinder.py
@@ -18,6 +18,7 @@ import collectd
 
 import base
 import collectd_openstack as openstack
+import re
 
 PLUGIN_NAME = 'cinder'
 INTERVAL = openstack.INTERVAL
@@ -26,12 +27,48 @@ INTERVAL = openstack.INTERVAL
 class CinderStatsPlugin(openstack.CollectdPlugin):
     """ Class to report the statistics on Cinder service.
 
+        state of agents
         number of volumes broken down by state
         total size of volumes usable and in error state
     """
 
     @base.read_callback_wrapper
     def read_callback(self):
+
+        # Get information of the state per service
+        # State can be up, down or disabled
+        # From API you get: status = 'disabled' or 'enabled'
+        #                   state = 'up' or 'down'
+        data = {}
+        cinder_re = re.compile('^cinder-')
+        os_services_r = self.get('cinder', 'os-services')
+        r_status = os_services_r.status_code
+        r_json = os_services_r.json()
+
+        if r_status == 200 and 'services' in r_json:
+            for val in r_json['services']:
+                service = cinder_re.sub('', val['binary'])
+                meta = {'host': val['host'].split('.')[0]}
+
+                if service not in data:
+                    data[service] = {}
+                    data[service] = {'up': 0, 'down': 0, 'disabled': 0}
+
+                if val['status'] == 'disabled':
+                    meta['state'] = 'disabled'
+                else:
+                    meta['state'] = val['state']
+
+                data[service][meta['state']] += 1
+                self.dispatch_value('cinder_service', service, 1, meta)
+
+            for key, val in data.iteritems():
+                for state, count in val.iteritems():
+                    meta = {'state': state}
+                    self.dispatch_value('cinder_services', key, count, meta)
+        else:
+            self.logger.warning("cinder state:{}:{}".format(r_status, r_json))
+
         volumes_details = self.get_objects_details('cinder', 'volumes')
 
         def groupby(d):
@@ -63,7 +100,7 @@ class CinderStatsPlugin(openstack.CollectdPlugin):
         for n, size in sizes.iteritems():
             self.dispatch_value('snapshots_size', n, size)
 
-    def dispatch_value(self, plugin_instance, name, value):
+    def dispatch_value(self, plugin_instance, name, value, meta={'0': True}):
         v = collectd.Values(
             plugin=PLUGIN_NAME,  # metric source
             plugin_instance=plugin_instance,
@@ -71,7 +108,7 @@ class CinderStatsPlugin(openstack.CollectdPlugin):
             type_instance=name,
             interval=INTERVAL,
             # w/a for https://github.com/collectd/collectd/issues/716
-            meta={'0': True},
+            meta=meta,
             values=[value]
         )
         v.dispatch()
