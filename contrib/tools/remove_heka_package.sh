9148ea3469370a221f60a8471dc6bf5d44c7cecd
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-03-25 15:07:55 +0100

    Add script to stop services and remove the Heka package
    This patch creates a new directory for StackLight tools that are scripts
    that can be used to perform actions or run diagnostics.
    It also adds two scripts. One for stopping StackLight services and
    another one to remove the Heka package on all nodes that are online.
    
    Change-Id: I93082ba3c7dfb75ec3ed44f5fa23a35599c4d163

---
+++ contrib/tools/remove_heka_package.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2016 Mirantis, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+NODES_LIST_FILE=/tmp/nodes
+
+. common.sh
+
+check_file_nodes_availability "${NODES_LIST_FILE}"
+
+# Remove Heka due to the issue with heka package versionning
+# https://github.com/mozilla-services/heka/issues/1892
+echo "** Remove Heka package"
+for n in $(grep True $NODES_LIST_FILE|grep ready|awk -F '|' '{print $5}'); do
+    echo "$n";
+    ssh "$n" 'apt-get remove -y heka'
+done
