5f75b89e9705ecfce4550a4015938503ec39b066
Swann Croiset <scroiset@mirantis.com>
2016-03-27 22:46:52 +0200

    Separate the (L)og of the LMA collector
    This change separates the processing of the logs/notifications and
    metric/alerting into 2 dedicated hekad processes, these services are
    named 'log_collector' and 'metric_collector'.
    
    Both services are managed by Pacemaker on controller nodes and by Upstart on
    other nodes.
    
    All metrics computed by log_collector (HTTP response times and creation time
    for instances and volumes) are sent directly to the metric_collector via TCP.
    Elasticsearch output (log_collector) uses full_action='block' and the
    TCP output uses full_action='drop'.
    
    All outputs of metric_collector (InfluxDB, HTTP and TCP) use
    full_action='drop'.
    
    The buffer size configurations are:
    * metric_collector:
      - influxdb-output buffer size is increased to 1.5Gb (vs 1Gb).
      - aggregator-output (tcp) buffer size is decreased to 256Mb (vs 1Gb).
      - nagios outputs (x3) buffer size are decreased to 1Mb.
    * log_collector:
      - elasticsearch-output buffer size is decreased to 256Mb (vs 1Gb).
      - tcp-output buffer size is set to 1Gb.
    
    Implements: blueprint separate-lma-collector-pipelines
    
    Change-Id: Ieadb93b89f81e944e21cf8e5a65f4d683fd0ffb8

--- deployment_scripts/puppet/manifests/base.pp
+++ deployment_scripts/puppet/manifests/base.pp
@@ -92,17 +92,18 @@ if $is_controller {
   # On controller nodes the log parsing can generate a lot of http_metrics
   # which can block heka (idle packs). It was observed that a poolsize set to 200
   # solves the issue.
-  $poolsize = 200
+  $log_poolsize = 200
 } else {
   # For other nodes, the poolsize is set to 100 (the Heka default value)
-  $poolsize = 100
+  $log_poolsize = 100
 }
 
 class { 'lma_collector':
-  tags     => $tags,
-  user     => $heka_user,
-  groups   => $additional_groups,
-  poolsize => $poolsize,
+  tags            => $tags,
+  user            => $heka_user,
+  groups          => $additional_groups,
+  log_poolsize    => $log_poolsize,
+  metric_poolsize => 100,
 }
 
 # On controller nodes the LMA collector service is managed by Pacemaker, so we
@@ -112,16 +113,18 @@ if $is_controller {
 
   include lma_collector::params
 
-  $service_name = $lma_collector::params::service_name
-  $config_dir = $lma_collector::params::config_dir
+  $log_service_name = $lma_collector::params::log_service_name
+  $metric_service_name = $lma_collector::params::metric_service_name
+  $log_config_dir = $lma_collector::params::log_config_dir
+  $metric_config_dir = $lma_collector::params::metric_config_dir
   $rabbitmq_resource = 'master_p_rabbitmq-server'
 
   if $fuel_version < 9.0 {
-    pacemaker_wrappers::service { $service_name:
+    pacemaker_wrappers::service { $log_service_name:
       ensure          => present,
       prefix          => false,
       primitive_class => 'ocf',
-      primitive_type  => 'ocf-lma_collector',
+      primitive_type  => 'ocf-log_collector',
       complex_type    => 'clone',
       use_handler     => false,
       ms_metadata     => {
@@ -132,8 +135,9 @@ if $is_controller {
         'failure-timeout'     => '120',
       },
       parameters      => {
-        'config'   => $config_dir,
-        'log_file' => "/var/log/${service_name}.log",
+        'config'   => $log_config_dir,
+        'log_file' => "/var/log/${log_service_name}.log",
+        'pid'      => "/var/run/${log_service_name}.pid",
         'user'     => $heka_user,
       },
       operations      => {
@@ -151,30 +155,64 @@ if $is_controller {
       ocf_script_file => 'lma_collector/ocf-lma_collector',
     }
 
-    cs_rsc_colocation { "${service_name}-with-rabbitmq":
+    cs_rsc_colocation { "${log_service_name}-with-rabbitmq":
       ensure     => present,
-      alias      => $service_name,
-      primitives => ["clone_${service_name}", $rabbitmq_resource],
+      alias      => $log_service_name,
+      primitives => ["clone_${log_service_name}", $rabbitmq_resource],
       score      => 0,
-      require    => Pacemaker_wrappers::Service[$service_name],
+      require    => Pacemaker_wrappers::Service[$log_service_name],
     }
 
-    cs_rsc_order { "${service_name}-after-rabbitmq":
+    cs_rsc_order { "${log_service_name}-after-rabbitmq":
       ensure  => present,
-      alias   => $service_name,
+      alias   => $log_service_name,
       first   => $rabbitmq_resource,
-      second  => "clone_${service_name}",
+      second  => "clone_${log_service_name}",
       # Heka cannot start if RabbitMQ isn't ready to accept connections. But
       # once it is initialized, it can recover from a RabbitMQ outage. This is
       # why we set score to 0 (interleave) meaning that the collector should
       # start once RabbitMQ is active but a restart of RabbitMQ
       # won't trigger a restart of the LMA collector.
       score   => 0,
-      require => Cs_rsc_colocation[$service_name],
+      require => Cs_rsc_colocation[$log_service_name],
       before  => Class['lma_collector'],
     }
+
+    pacemaker_wrappers::service { $metric_service_name:
+      ensure          => present,
+      prefix          => false,
+      primitive_class => 'ocf',
+      primitive_type  => 'ocf-metric_collector',
+      complex_type    => 'clone',
+      use_handler     => false,
+      ms_metadata     => {
+        # The resource can start at any time
+        'interleave'          => false,
+        'migration-threshold' => '3',
+        'failure-timeout'     => '120',
+      },
+      parameters      => {
+        'config'   => $metric_config_dir,
+        'log_file' => "/var/log/${metric_service_name}.log",
+        'pid'      => "/var/run/${metric_service_name}.pid",
+        'user'     => $heka_user,
+      },
+      operations      => {
+        'monitor' => {
+          'timeout'  => '10',
+        },
+        'start'   => {
+          'timeout' => '30',
+        },
+        'stop'    => {
+          'timeout' => '30',
+        },
+      },
+      ocf_script_file => 'lma_collector/ocf-lma_collector',
+    }
+
   } else {
-    pacemaker::service { $service_name:
+    pacemaker::service { $log_service_name:
       ensure           => present,
       prefix           => false,
       primitive_class  => 'ocf',
@@ -189,8 +227,9 @@ if $is_controller {
         'failure-timeout'     => '120',
       },
       parameters       => {
-        'config'   => $config_dir,
-        'log_file' => "/var/log/${service_name}.log",
+        'config'   => $log_config_dir,
+        'log_file' => "/var/log/${log_service_name}.log",
+        'pid'      => "/var/run/${log_service_name}.pid",
         'user'     => $heka_user,
       },
       operations       => {
@@ -208,17 +247,51 @@ if $is_controller {
       ocf_script_file  => 'lma_collector/ocf-lma_collector',
     }
 
-    pcmk_colocation { "${service_name}-with-rabbitmq":
+    pcmk_colocation { "${log_service_name}-with-rabbitmq":
       ensure  => present,
-      alias   => $service_name,
+      alias   => $log_service_name,
       first   => $rabbitmq_resource,
-      second  => "clone_${service_name}",
+      second  => "clone_${log_service_name}",
       score   => 0,
-      require => Pacemaker::Service[$service_name],
+      require => Pacemaker::Service[$log_service_name],
+    }
+
+    pacemaker::service { $metric_service_name:
+      ensure           => present,
+      prefix           => false,
+      primitive_class  => 'ocf',
+      primitive_type   => 'ocf-lma_collector',
+      use_handler      => false,
+      complex_type     => 'clone',
+      complex_metadata => {
+        # The resource can start at any time
+        'interleave'          => false,
+        'migration-threshold' => '3',
+        'failure-timeout'     => '120',
+      },
+      parameters       => {
+        'config'   => $metric_config_dir,
+        'log_file' => "/var/log/${metric_service_name}.log",
+        'pid'      => "/var/log/${metric_service_name}.pid",
+        'user'     => $heka_user,
+      },
+      operations       => {
+        'monitor' => {
+          'timeout'  => '10',
+        },
+        'start'   => {
+          'timeout' => '30',
+        },
+        'stop'    => {
+          'timeout' => '30',
+        },
+      },
+      ocf_script_file  => 'lma_collector/ocf-lma_collector',
     }
   }
 }
 
+$influxdb_mode = $lma_collector['influxdb_mode']
 if $elasticsearch_mode != 'disabled' {
   class { 'lma_collector::logs::system':
     require => Class['lma_collector'],
@@ -235,9 +308,14 @@ if $elasticsearch_mode != 'disabled' {
     server  => $es_server,
     require => Class['lma_collector'],
   }
+
+  if $influxdb_mode != 'disabled'{
+    class { 'lma_collector::metrics::tcp_output':
+      require => Class['lma_collector'],
+    }
+  }
 }
 
-$influxdb_mode = $lma_collector['influxdb_mode']
 case $influxdb_mode {
   'remote','local': {
     if $influxdb_mode == 'remote' {
@@ -281,6 +359,10 @@ case $influxdb_mode {
     }
 
     class { 'lma_collector::metrics::heka_monitoring':
+      require => Class['lma_collector']
+    }
+
+    class { 'lma_collector::metrics::tcp_input':
       require => Class['lma_collector']
     }
 
