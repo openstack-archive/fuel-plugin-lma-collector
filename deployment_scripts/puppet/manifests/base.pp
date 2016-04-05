28de5358e8842d253c9e5c04d44e28cc77534fc0
Swann Croiset <scroiset@mirantis.com>
2016-04-05 09:53:24 +0200

    Increase the Heka poolsize on controllers
    On controller nodes, the Heka poolsize must be increased to handle the load
    generated by derived metrics from logs otherwise a deadlock
    can happen in the filter plugins and block heka.
    
    Fixes-bug: #1557388
    (cherry picked from commit 96df47af7385578280a68863b55416da7dd8bfdb)
    
    Conflicts:
    	deployment_scripts/puppet/manifests/base.pp
    	deployment_scripts/puppet/modules/lma_collector/manifests/init.pp
    
    Change-Id: I74362011d32d413f244c6cdb6e4625ed96759df0

--- deployment_scripts/puppet/manifests/base.pp
+++ deployment_scripts/puppet/manifests/base.pp
@@ -73,6 +73,16 @@ case $elasticsearch_mode {
 }
 
 # Notifications are always collected even when event indexation is disabled
+if $is_controller {
+  # On controller nodes the log parsing can generate a lot of http_metrics
+  # which can block heka (idle packs). It was observed that a poolsize set to 200
+  # solves the issue.
+  $poolsize = 200
+} else {
+  # For other nodes, the poolsize is set to 100 (the Heka default value)
+  $poolsize = 100
+}
+
 if $is_controller{
   #$pre_script        = '/usr/local/bin/wait_for_rabbitmq'
   # Params used by the script.
@@ -100,6 +110,7 @@ class { 'lma_collector':
   groups            => $additional_groups,
   pacemaker_managed => $pacemaker_managed,
   rabbitmq_resource => $rabbitmq_resource,
+  poolsize          => $poolsize,
 }
 
 if $elasticsearch_mode != 'disabled' {
