e9c9c691fd2437a61470f3fc57e60818e5d5c13e
Swann Croiset <scroiset@mirantis.com>
2016-04-20 18:05:36 +0200

    Emit aggregated HTTP metrics
    HTTP metrics are now statistics aggregated over 10 second intervals.
    A new metric is emitted openstack_<service>_response_times with these
    values:
    - min
    - max
    - sum
    - count
    - percentile
    
    Hence, the previous metric disappears (openstack_<service>_responses).
    
    Implements-blueprint: aggregated-http-metrics
    
    Change-Id: I48e92df6f4baa7be942ad138b7f23c3d15f5a24e

--- deployment_scripts/puppet/modules/lma_collector/manifests/logs/aggregated_http_metrics.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/logs/aggregated_http_metrics.pp
@@ -12,17 +12,28 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 #
-class lma_collector::logs::http_metrics {
+class lma_collector::logs::aggregated_http_metrics (
+  $interval = 10,
+  $hostname = $::hostname,
+  $bulk_size = 500,
+  $max_timer_inject = $lma_collector::params::hekad_max_timer_inject,
+  $percentile = 90,
+) inherits lma_collector::params {
 
-  include lma_collector::params
   include lma_collector::service::log
 
-  # This sandbox has been replaced by the aggregated_http_metrics one.
-  heka::filter::sandbox { 'http_metrics':
-    ensure          => absent,
+  heka::filter::sandbox { 'aggregated_http_metrics':
     config_dir      => $lma_collector::params::log_config_dir,
-    filename        => "${lma_collector::params::plugins_dir}/filters/http_metrics.lua",
+    filename        => "${lma_collector::params::plugins_dir}/filters/http_metrics_aggregator.lua",
     message_matcher => 'Type == \'log\' && Fields[http_response_time] != NIL',
+    ticker_interval => $interval,
+    config          => {
+      hostname         => $hostname,
+      interval         => $interval,
+      max_timer_inject => $max_timer_inject,
+      bulk_size        => $bulk_size,
+      percentile       => $percentile,
+    },
     notify          => Class['lma_collector::service::log'],
   }
 }
