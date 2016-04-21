d48d29884f4784d0d3b830ecd493602f04a770e5
Swann Croiset <scroiset@mirantis.com>
2016-04-21 14:34:33 +0200

    Add metric TCP decoder for the metric_collector
    This patch adds a custom decoder to be able to deserialize bulk_metrics.
    The deserialization of bulk_metric message is configurable per Logger and
    is disabled by default.
    Only the HTTP metrics are deserialized (Logger: aggregated_http_metrics_filter)
    
    blueprint: keystone-performance-monitoring
    
    Change-Id: I08e8c816d889f4b0917d766b000fb25330eff174

---
+++ deployment_scripts/puppet/modules/lma_collector/files/plugins/decoders/metric.lua
@@ -0,0 +1,80 @@
+-- Copyright 2016 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+require "string"
+require "cjson"
+
+local deserialize_bulk_metric_for_loggers = read_config("deserialize_bulk_metric_for_loggers") or false
+
+local utils = require 'lma_utils'
+
+function process_message ()
+    local msg = decode_message(read_message("raw"))
+    if deserialize_bulk_metric_for_loggers and
+       string.match(msg.Type, 'bulk_metric$') and
+       string.match(msg.Logger, deserialize_bulk_metric_for_loggers) then
+
+        local ok, metrics = pcall(cjson.decode, msg.Payload)
+        if not ok then
+            return -1
+        end
+        for _, metric in ipairs(metrics) do
+            local fields = {}
+            local metric_type
+            if metric.value then
+                metric_type = 'metric'
+                fields['value'] = metric.value
+            else
+                metric_type = 'multivalue_metric'
+                local value_fields = {}
+                for k, v in pairs(metric.values) do
+                    fields[k] = v
+                    table.insert(value_fields, k)
+                end
+                fields['value_fields'] = value_fields
+            end
+            local tag_fields = {}
+            for t, v in pairs(metric.tags or {}) do
+                fields[t] = v
+                table.insert(tag_fields, t)
+            end
+            fields['tag_fields'] = tag_fields
+            fields['name'] = metric.name
+            fields['hostname'] = msg.Hostname
+
+            local new_msg = {
+                Timestamp = msg.Timestamp,
+                Hostname = msg.Hostname,
+                Severity = msg.Severity,
+                Logger = msg.Logger,
+                Type = metric_type,
+                Payload = '',
+                Fields = fields,
+            }
+
+            utils.inject_tags(new_msg)
+            ok, err = utils.safe_inject_message(new_msg)
+            if ok ~= 0 then
+                return -1, err
+            end
+        end
+    else -- simple metric
+        utils.inject_tags(msg)
+        ok, err = utils.safe_inject_message(msg)
+        if ok ~= 0 then
+            return -1, err
+        end
+    end
+    return 0
+end
+
--- deployment_scripts/puppet/modules/lma_collector/manifests/heka.pp
+++ deployment_scripts/puppet/modules/lma_collector/manifests/heka.pp
@@ -61,12 +61,23 @@ define lma_collector::heka (
     $service_class = 'lma_collector::service::metric'
     $dashboard_port = $lma_collector::params::metric_dashboard_port
 
+    heka::decoder::sandbox { 'metric':
+      config_dir       => $config_dir,
+      filename         => "${lma_collector::params::plugins_dir}/decoders/metric.lua",
+      module_directory => $lua_modules_dir,
+      # NOTE: to deserialize 'bulk_metric' messages, a string pattern (compliant with Lua 'string.match')
+      # must be provided to the decoder to match the Logger message field.
+      # For example for HTTP metrics:
+      config           => {'deserialize_bulk_metric_for_loggers' => 'aggregated_http_metrics_filter'},
+      notify           => Class[$service_class],
+    }
+
     heka::input::tcp { 'metric':
       config_dir => $config_dir,
       address    => $lma_collector::params::metric_input_address,
       port       => $lma_collector::params::metric_input_port,
-      decoder    => 'ProtobufDecoder',
-      require    => ::Heka[$title],
+      decoder    => 'metric',
+      require    => [::Heka[$title], Heka::Decoder::Sandbox['metric']],
       notify     => Class[$service_class],
     }
 
