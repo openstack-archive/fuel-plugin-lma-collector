6dc87065fb8bbce1bf007517b2cdaec0897ea274
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-04-26 11:49:24 +0200

    Support for the detach-rabbitmq plugin
    This patch adds the support when the RabbitMQ cluster is deployed on
    dedicated nodes [1].
    
    [1] https://github.com/openstack/fuel-plugin-detach-rabbitmq
    
    Change-Id: Icc337e48d9a836ccab85dfc0b8ca86ff58c5cd4d
    Closes-Bug: #1547086
    Closes-Bug: #1575046

--- deployment_scripts/puppet/manifests/configure_afd_filters.pp
+++ deployment_scripts/puppet/manifests/configure_afd_filters.pp
@@ -19,14 +19,15 @@ include lma_collector::params
 $lma           = hiera_hash('lma_collector', {})
 $roles         = node_roles(hiera('nodes'), hiera('uid'))
 $is_controller = member($roles, 'controller') or member($roles, 'primary-controller')
+$is_rabbitmq   = roles_include(['standalone-rabbitmq', 'primary-standalone-rabbitmq'])
 
 $alarms_definitions = $lma['alarms']
 if $alarms_definitions == undef {
     fail('Alarms definitions not found. Check files in /etc/hiera/override.')
 }
 
-if $is_controller {
-  # On controllers make sure the Log and Metric collector services are
+if $is_controller or $is_rabbitmq {
+  # On nodes where pacemaker is deployed, make sure the LMA service is
   # configured with the "pacemaker" provider
   include lma_collector::params
   Service<| title == $lma_collector::params::log_service_name |> {
