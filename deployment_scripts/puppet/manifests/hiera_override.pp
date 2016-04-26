433e76facb3a7b5284171758f01ddb6b21ef2749
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-04-26 11:49:24 +0200

    Support for detach-rabbitmq
    This patch adds the support when rabbitmq is running on another node
    than controller.
    
    Change-Id: Icc337e48d9a836ccab85dfc0b8ca86ff58c5cd4d
    Closes-Bug: #1547086
    Closes-Bug: #1575046

--- deployment_scripts/puppet/manifests/hiera_override.pp
+++ deployment_scripts/puppet/manifests/hiera_override.pp
@@ -23,6 +23,14 @@ if ($plugin_data) {
   $ceilometer_enabled = pick($ceilometer['enabled'], false)
   $contrail_plugin = hiera('contrail', false)
 
+  # detach_rabbitmq_enabled is used in alarming.yaml.erb
+  $detach_rabbitmq = hiera('detach-rabbitmq', {})
+  if $detach_rabbitmq['metadata'] {
+    $detach_rabbitmq_enabled = $detach_rabbitmq['metadata']['enabled']
+  } else {
+    $detach_rabbitmq_enabled = false
+  }
+
   $elasticsearch_mode = $plugin_data['elasticsearch_mode']
   $monitor_elasticsearch = $elasticsearch_mode ? {
     'local' => true,
