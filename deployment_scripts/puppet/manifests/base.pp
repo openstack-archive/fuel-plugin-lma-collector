1585aef93f161ebd365dafab035c37c223b349d6
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-04-27 14:21:26 +0200

    Support for the detach-database plugin
    This patch adds the support when the database is deployed on a dedicated
    node [1].
    
    [1] https://github.com/openstack/fuel-plugin-detach-database
    
    Change-Id: If800d9d09204a1456640863a3ed3c5dc66d29017
    Closes-Bug: #1547089

--- deployment_scripts/puppet/manifests/base.pp
+++ deployment_scripts/puppet/manifests/base.pp
@@ -25,13 +25,19 @@ $current_node_name = hiera('user_node_name')
 $current_roles     = hiera('roles')
 $network_metadata  = hiera_hash('network_metadata')
 $detach_rabbitmq   = hiera('detach-rabbitmq', {})
+$detach_database   = hiera('detach-database', {})
+
+if $detach_database['metadata'] and $detach_database['metadata']['enabled'] {
+  $is_mysql_server = member($roles, 'standalone-database') or member($roles, 'primary-standalone-database')
+} else {
+  $is_mysql_server = $is_controller
+}
 
 if $detach_rabbitmq['metadata'] and $detach_rabbitmq['metadata']['enabled'] {
   $is_rabbitmq = member($roles, 'standalone-rabbitmq')
 } else {
   $is_rabbitmq = $is_controller
 }
-
 
 $elasticsearch_kibana = hiera_hash('elasticsearch_kibana', {})
 $es_nodes = get_nodes_hash_by_roles($network_metadata, ['elasticsearch_kibana'])
@@ -246,6 +252,10 @@ if $elasticsearch_mode != 'disabled' {
     require => Class['lma_collector'],
   }
 
+  if $is_mysql_server {
+    class { 'lma_collector::logs::mysql': }
+  }
+
   if $is_rabbitmq {
     class { 'lma_collector::logs::rabbitmq': }
   }
@@ -274,14 +284,28 @@ case $influxdb_mode {
       $influxdb_password = $influxdb_grafana['influxdb_userpass']
     }
 
-    if ! $is_controller {
+    # TODO(all):  this class is also applied by other role-specific manifests.
+    # This is sub-optimal and error prone. It needs to be fixed by having all
+    # collectd resources managed by a single manifest.
+    class { 'lma_collector::collectd::base':
+      processes    => ['hekad', 'collectd'],
+      # Purge the default configuration shipped with the collectd package
+      purge        => true,
+      require      => Class['lma_collector'],
+    }
 
-      class { 'lma_collector::collectd::base':
-        processes    => ['hekad', 'collectd'],
-        read_threads => 5,
-        # Purge the default configuration shipped with the collectd package
-        purge        => true,
-        require      => Class['lma_collector'],
+    if $is_mysql_server {
+      $nova = hiera_hash('nova', {})
+
+      class { 'lma_collector::collectd::mysql':
+        username => 'nova',
+        password => $nova['db_password'],
+      }
+
+      lma_collector::collectd::dbi_mysql_status { 'mysql_status':
+        username => 'nova',
+        dbname   => 'nova',
+        password => $nova['db_password'],
       }
     }
 
