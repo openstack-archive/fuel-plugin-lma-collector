a69dd0d0f82c4ec4503ae2e18987dfa06e44dcdb
Igor Degtiarov <idegtiarov@mirantis.com>
2016-08-17 12:59:37 +0300

    Add swap_used_pct metric
    Change-Id: I1ac8dc82ecfd9c52ceaa58fbe06edfcea9576a05

--- deployment_scripts/puppet/modules/lma_collector/files/plugins/decoders/collectd.lua
+++ deployment_scripts/puppet/modules/lma_collector/files/plugins/decoders/collectd.lua
@@ -34,6 +34,7 @@ local processes_map = {
 -- this is needed for the libvirt metrics because in that case, collectd sends
 -- the instance's ID instead of the hostname in the 'host' attribute
 local hostname = read_config('hostname') or error('hostname must be specified')
+local swap_size = read_config('swap_size') + 0
 
 function replace_dot_by_sep (str)
     return string.gsub(str, '%.', sep)
@@ -352,6 +353,13 @@ function process_message ()
                 msg['Fields']['name'] = metric_source
                 msg['Fields']['service'] = sample['type_instance']
                 msg['Fields']['tag_fields'] = { 'service' }
+            elseif metric_source == 'swap' and metric_name == 'swap_used' and swap_size > 0 then
+                msg['Fields']['name'] = 'swap_percent_used'
+                msg['Fields']['value'] = value / swap_size
+                utils.inject_tags(msg)
+                utils.safe_inject_message(msg)
+                msg['Fields']['name'] = metric_name
+                msg['Fields']['value'] = value
             else
                 msg['Fields']['name'] = replace_dot_by_sep(metric_name)
             end
