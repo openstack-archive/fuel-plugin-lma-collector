8ef08aea640565d39dd4821dd7d37d2414f7e26e
Guillaume Thouvenin <gthouvenin@mirantis.com>
2016-09-13 13:53:04 +0200

    Fix issue when installing the OCF script
    This patch moves the installation of the OCF script at the beginning of
    the depoy_start to be sure that it is available when pacemaker starts
    the collector resources. As it requires a configured hiera we also moved
    the hiera task.
    
    Change-Id: I90b4fa2a9038eaed0f1dcadb0f00713a1b2487b0
    Closes-bug: #1575039

--- deployment_scripts/puppet/manifests/install_ocf_script.pp
+++ deployment_scripts/puppet/manifests/install_ocf_script.pp
@@ -1,4 +1,4 @@
-# Copyright 2015 Mirantis, Inc.
+# Copyright 2016 Mirantis, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
@@ -12,20 +12,16 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-notice('fuel-plugin-lma-collector: configure_apt.pp')
+notice('fuel-plugin-lma-collector: install_ocf_scripts.pp')
 
-$str = 'APT::Install-Suggests "0";
-APT::Install-Recommends "0";
-'
-
-case $::osfamily {
-    'Debian': {
-        file { '/etc/apt/apt.conf.d/99norecommends':
-            ensure  => file,
-            content => $str,
-        }
-    }
-    default: {
-        # Currently only Debian like distributions need specific configuration.
-    }
+$node_profiles   = hiera_hash('lma::collector::node_profiles')
+if $node_profiles['controller'] or $node_profiles['rabbitmq'] or $node_profiles['mysql'] {
+  file { 'ocf-lma_collector':
+    ensure => present,
+    source => 'puppet:///modules/lma_collector/ocf-lma_collector',
+    path   => '/usr/lib/ocf/resource.d/fuel/ocf-lma_collector',
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+  }
 }
