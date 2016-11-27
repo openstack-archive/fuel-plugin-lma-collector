6537e3bc7025c7613406d4d492a96da8c3dcd792
Ildar Svetlov <isvetlov@mirantis.com>
2016-11-27 12:10:31 +0300

    Alarms state tcp notifier
    This patch adds new feature:
    Sending events about alarms state changing (both AFD and GSE).
    Output: TCP + RstEncoder
    
    Change-Id: Ia5c454f911135d2ee0a4230641d2e3945c3bcf1c

---
+++ deployment_scripts/puppet/modules/lma_collector/manifests/alarms_tcp.pp
@@ -0,0 +1,46 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+class lma_collector::gse_afd_tcp_notifier (
+) inherits lma_collector::params {
+
+  include lma_collector::service::metric
+
+  $lua_modules_dir = $lma_collector::params::lua_modules_dir
+
+  heka::filter::sandbox { 'gse_afd_notification':
+    config_dir       => $lma_collector::params::metric_config_dir,
+    filename         => "${lma_collector::params::plugins_dir}/filters/gse_afd_tcp_notifier.lua",
+    message_matcher  => "(Type =~ /heka.sandbox.gse_.*metric$/ || Type =~ /heka.sandbox.afd_.*metric$/) && Fields[aggregator] == NIL",
+    module_directory => $lua_modules_dir,
+    notify           => Class['lma_collector::service::metric'],
+  }
+
+  heka::encoder::rst { 'rst':
+    config_dir => $lma_collector::params::metric_config_dir,
+    notify     => Class['lma_collector::service::metric'],
+  }
+
+  heka::output::tcp { 'tcp_notification':
+      config_dir        => $lma_collector::params::metric_config_dir,
+      address           => '127.0.0.1',
+      port              => 40000,
+      message_matcher   => 'Type == \'heka.sandbox.alarm_notification\'',
+      max_buffer_size   => 256 * 1024 * 1024,
+      max_file_size     => 64 * 1024 * 1024,
+      queue_full_action => 'drop',
+      notify            => Class['lma_collector::service::metric'],
+      encoder           => 'rst_encoder'
+    }
+}
