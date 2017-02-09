39ab2391b71d085c56272f2e154ef2c85a5041e5
Danil Akhmetov <dakhmetov@mirantis.com>
2017-02-09 16:54:05 +0300

    Always use Keystone v3 API obtaining users and projects in order to list all users and tenants in multi-domain environments.
    Change-Id: If94ad4a0d61d0467031b384d132eb433682b3a20
    Closes-Bug: #1662142

--- deployment_scripts/puppet/modules/lma_collector/files/collectd/collectd_openstack.py
+++ deployment_scripts/puppet/modules/lma_collector/files/collectd/collectd_openstack.py
@@ -170,11 +170,10 @@ class CollectdPlugin(base.Base):
     def _build_url(self, service, resource):
         s = (self.get_service(service) or {})
         # the adminURL must be used to access resources with Keystone API v2
+        # v3 API must be used in order to obtain tenants in multi-domain envs
         if service == 'keystone' and \
-                (resource in ['tenants', 'users'] or 'OS-KS' in resource):
-            url = s.get('admin_url')
-        else:
-            url = s.get('url')
+                (resource in ['projects', 'users'] or 'OS-KS' in resource):
+            url = s.get('admin_url').replace('v2.0', 'v3')
 
         if url:
             if url[-1] != '/':
