87ef00e6d025ce45715fa4c82987f705eb9f126a
Jim Rollenhagen <jim@jimrollenhagen.com>
2019-09-26 10:42:23 -0400

    Retire github mirror, repo moved to opendev

--- contrib/README.md
+++
@@ -1,2 +0,0 @@
-This directory contains various scripts and tools that help with the deployment
-and management of the StackLight toolchain. These materials are provided as-is.
--- deployment_scripts/puppet/modules/lma_collector/spec/functions/sanitize_name_for_lua_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'sanitize_name_for_lua' do
-    it { is_expected.not_to eq(nil) }
-    it { is_expected.to run.with_params().and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
-    it { is_expected.to run.with_params('un', 'deux').and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
-    it { is_expected.to run.with_params('').and_return('')}
-    it { is_expected.to run.with_params('one-two-three').and_return('one_two_three')}
-end
